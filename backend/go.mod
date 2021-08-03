c0bc6cb22d560157859792b74627307b39537f0d
Jonathan West <jgwest@gmail.com>
2021-08-02 22:19:06 -0400

    Initial commit of backend

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/jgwest/argocdaas-backend
+
+go 1.14
+
+require github.com/emicklei/go-restful/v3 v3.3.3
---
+++ backend/restful-basic-authentication.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	restful "github.com/emicklei/go-restful/v3"
+)
+
+// This example shows how to create a (Route) Filter that performs Basic Authentication on the Http request.
+//
+// GET http://localhost:8080/secret
+// and use admin,admin for the credentials
+
+func main() {
+	ws := new(restful.WebService)
+	// ws.Consumes(restful.MIME_JSON, restful.MIME_XML)
+	// ws.Produces(restful.MIME_JSON, restful.MIME_XML)
+
+	ws.Route(ws.GET("/"). /*.Filter(basicAuthenticate)*/ To(secret))
+	// ws.Param(ws.PathParameter("medium", "digital or paperback").DataType("string")).
+	// ws.Param(ws.QueryParameter("language", "en,nl,de").DataType("string")).
+	// ws.Param(ws.HeaderParameter("If-Modified-Since", "last known timestamp").DataType("datetime")).
+	restful.Add(ws)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// func basicAuthenticate(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+// 	// usr/pwd = admin/admin
+// 	u, p, ok := req.Request.BasicAuth()
+// 	if !ok || u != "admin" || p != "admin" {
+// 		resp.AddHeader("WWW-Authenticate", "Basic realm=Protected Area")
+// 		resp.WriteErrorString(401, "401: Not Authorized")
+// 		return
+// 	}
+// 	chain.ProcessFilter(req, resp)
+// }
+
+func secret(req *restful.Request, resp *restful.Response) {
+
+	io.WriteString(resp, "42")
+}

1b347685339ced48348bc8cffb40ffc482567e16
Samyak Jain <samyak.jn11@gmail.com>
2021-09-09 12:29:10 +0530

    Adds POST, GET method for operations along with the relevant tests
    Restructuring routes/opr*.go -> routes/operations/opr*.go

---
+++ backend/routes/operations/operations.go
@@ -0,0 +1,74 @@
+package routes
+
+import (
+	"net/http"
+
+	restful "github.com/emicklei/go-restful/v3"
+)
+
+/*
+Operation
+
+/api/v1/operation
+POST: Create a new operation
+
+/api/v1/operation/(id)
+GET: Retrieve the given operation
+*/
+
+// Creating a REST layer as OperationResource to have all the operation
+
+type Operation struct {
+	Id, Name string
+}
+
+type OperationResource struct {
+	operations map[string]Operation
+}
+
+// Creating a webservice for operation endpoints
+func (o OperationResource) Register(container *restful.Container) {
+	ws := new(restful.WebService)
+	ws.
+		Path("/api/v1/operation").
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+
+	ws.Route(ws.GET("/{operation-id}").To(o.findOperation))
+	ws.Route(ws.POST("").To(o.addOperation))
+	container.Add(ws)
+}
+
+// GET info of all the operations
+func (o OperationResource) findAllOperations(request *restful.Request, response *restful.Response) {
+	list := []Operation{}
+	for _, each := range o.operations {
+		list = append(list, each)
+	}
+	response.WriteEntity(list)
+}
+
+// GET info of operations depening upon the id
+func (o OperationResource) findOperation(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("operation-id")
+	opr := o.operations[id]
+	if len(opr.Id) == 0 {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusNotFound, "Operation not found!")
+	} else {
+		response.WriteEntity(opr)
+	}
+}
+
+// POST to create an operation
+func (o *OperationResource) addOperation(request *restful.Request, response *restful.Response) {
+	opr := new(Operation)
+	err := request.ReadEntity(&opr)
+	if err == nil {
+		o.operations[opr.Id] = *opr
+		response.WriteEntity(opr)
+	} else {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+	}
+}
---
+++ backend/routes/operations/operations_test.go
@@ -0,0 +1,41 @@
+func TestServer(t *testing.T) {
+	serverURL := "http://localhost:8090"
+	go func() {
+		RunRestfulCurlyRouterServer()
+	}()
+	if err := waitForServerUp(serverURL); err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// GET should give a 405
+	resp, err := http.Get(serverURL + "/operations/")
+	if err != nil {
+		t.Errorf("unexpected error in GET /operations/: %v", err)
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("unexpected response: %v, expected: %v", resp.StatusCode, http.StatusOK)
+	}
+
+	// Send a POST request.
+	var jsonStr = []byte(`{"id":"1","name":"operation1"}`)
+	req, err := http.NewRequest("POST", serverURL+"/operations/", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", restful.MIME_JSON)
+
+	client := &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("unexpected error in sending req: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("unexpected response: %v, expected: %v", resp.StatusCode, http.StatusOK)
+	}
+
+	// Test that GET works.
+	resp, err = http.Get(serverURL + "/operations/1")
+	if err != nil {
+		t.Errorf("unexpected error in GET /operations/1: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("unexpected response: %v, expected: %v", resp.StatusCode, http.StatusOK)
+	}
+}

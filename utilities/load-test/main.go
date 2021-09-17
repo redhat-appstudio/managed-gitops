29ca1887aae1755e80bae6554f7e47d4ee3aecd3
Samyak Jain <samyak.jn11@gmail.com>
2021-09-17 17:39:16 +0530

    Creates a utility for kubectl apply command to apply argoCD yamls
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

---
+++ utilities/load-test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	utils "github.com/redhat-appstudio/managed-gitops/utilities/load-test/loadtest"
+)
+
+func main() {
+	// Running a kubectl apply command by passing namespace and URL for the manifests yaml(s)
+	utils.Kubectl_apply("argocd", "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml")
+
+}

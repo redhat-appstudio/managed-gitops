cecf92b74fac30f3cf141bd077d1c5d6e5bec519
Samyak Jain <samyak.jn11@gmail.com>
2021-10-20 17:59:17 +0530

    Add a load test which consists of deploying Heavy Application simultaneously
    Minor refractoring a/c to go best practices
    
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- utilities/load-test/loadtest/loadtest_test.go
+++ utilities/load-test/loadtest/loadtest_test.go
@@ -28,14 +28,14 @@ var (
 
 func TestNGuestbook(t *testing.T) {
 	// TEST 1: Deply 100 guestbook in different namespace
-	fmt.Print("Testing for 100 Guestbook applications\n")
+	t.Log("Testing for 100 Guestbook applications\n")
 
-	fmt.Print("\n\nPods Memory Resource before creation:\n\n")
-	memoryInit := Get_pod_info(kubeconfig, master, namespace, podName)
+	t.Log("\n\nPods Memory Resource before creation:\n\n")
+	memoryInit := GetPodInfo(kubeconfig, master, namespace, podName)
 	PodInfoParse(memoryInit)
 
 	// Get Pod Restart Count
-	PodC := PodRestartcount(namespace)
+	PodC := PodRestartCount(namespace)
 
 	for i := 1; i < 101; i++ {
 		guestbookApp := &appv1.Application{
@@ -54,7 +54,7 @@ func TestNGuestbook(t *testing.T) {
 					RepoURL:        "https://github.com/argoproj/argocd-example-apps.git",
 				},
 				Destination: appv1.ApplicationDestination{
-					Namespace: "guestbook-" + fmt.Sprintf("%d", i),
+					Namespace: "guestbook",
 					Server:    "https://kubernetes.default.svc",
 				},
 				Project: "default",
@@ -69,16 +69,16 @@ func TestNGuestbook(t *testing.T) {
 	}
 
 	// Check if the Pod is getting refreshed or not
-	if reflect.DeepEqual(PodC, PodRestartcount(namespace)) != true {
+	if reflect.DeepEqual(PodC, PodRestartCount(namespace)) != true {
 		t.Errorf("error, %v", "The Pods are getting refreshed!")
 		return
 	}
 
-	fmt.Print("\n\nPods Memory Resource after Creation:\n\n")
-	memoryPost := Get_pod_info(kubeconfig, master, namespace, podName)
+	t.Log("\n\nPods Memory Resource after Creation:\n\n")
+	memoryPost := GetPodInfo(kubeconfig, master, namespace, podName)
 	PodInfoParse(memoryPost)
 
-	fmt.Print("\n\nDifference in the Pod Memory Usage (in Ki)\n\n")
+	t.Log("\n\nDifference in the Pod Memory Usage (in Ki)\n\n")
 	delta := PodMemoryDiff(memoryInit, memoryPost)
 	PodInfoParse(delta)
 
@@ -95,167 +95,167 @@ func TestNGuestbook(t *testing.T) {
 			return
 		}
 	}
-	fmt.Println("100 guestbook applications are created, deployed and deleted successfully!")
+	t.Log("100 guestbook applications are created, deployed and deleted successfully!")
 }
 
-// func TestHeavyApplication(t *testing.T) {
+func TestHeavyApplication(t *testing.T) {
 
-// 	// TEST 2: Deply Prometheus + External DNS + Cert Manager application
+	// TEST 2: Deply Prometheus + External DNS + Cert Manager application
 
-// 	fmt.Print("Testing for Prometheus + External DNS + Cert Manager applications\n")
+	t.Log("Testing for Prometheus + External DNS + Cert Manager applications\n")
 
-// 	fmt.Print("\n\nPods Memory Resource before creation:\n\n")
-// 	// Intial Memory Information
-// 	memoryInit := Get_pod_info(kubeconfig, master, namespace, podName)
-// 	PodInfoParse(memoryInit)
-
-// 	// Get Pod Restart Count
-// 	PodC := PodRestartcount(namespace)
-
-// 	PrometheusApp := &appv1.Application{
-// 		TypeMeta: v1.TypeMeta{
-// 			Kind:       "Application",
-// 			APIVersion: "argoproj.io/v1alpha1",
-// 		},
-// 		ObjectMeta: v1.ObjectMeta{
-// 			Name:      "prometheus-app",
-// 			Namespace: "argocd",
-// 		},
-// 		Spec: appv1.ApplicationSpec{
-// 			Source: appv1.ApplicationSource{
-// 				RepoURL: "https://github.com/jgwest/argocd-example-apps.git",
-// 				Path:    "appset-examples/cluster-addon/prometheus-operator",
-// 				Helm: &appv1.ApplicationSourceHelm{
-// 					Version: "v2",
-// 				},
-// 			},
-// 			Destination: appv1.ApplicationDestination{
-// 				Namespace: "prometheus-app",
-// 				Server:    "https://kubernetes.default.svc",
-// 			},
-// 			Project: "default",
-// 		},
-// 	}
-
-// 	_, err_createPrometheus := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Create(context.TODO(), PrometheusApp, v1.CreateOptions{})
-// 	if err_createPrometheus != nil {
-// 		t.Errorf("error, %v", err_createPrometheus)
-// 		return
-// 	}
-
-// 	CertManagerApp := &appv1.Application{
-// 		TypeMeta: v1.TypeMeta{
-// 			Kind:       "Application",
-// 			APIVersion: "argoproj.io/v1alpha1",
-// 		},
-// 		ObjectMeta: v1.ObjectMeta{
-// 			Name:      "cert-manager-app",
-// 			Namespace: "argocd",
-// 		},
-// 		Spec: appv1.ApplicationSpec{
-// 			Source: appv1.ApplicationSource{
-// 				RepoURL: "https://github.com/jgwest/argocd-example-apps.git",
-// 				Path:    "cert-manager",
-// 				Helm: &appv1.ApplicationSourceHelm{
-// 					Version: "v2",
-// 				},
-// 			},
-// 			Destination: appv1.ApplicationDestination{
-// 				Namespace: "cert-manager-app",
-// 				Server:    "https://kubernetes.default.svc",
-// 			},
-// 			Project: "default",
-// 		},
-// 	}
-
-// 	_, err_createCertMan := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Create(context.TODO(), CertManagerApp, v1.CreateOptions{})
-// 	if err_createCertMan != nil {
-// 		t.Errorf("error, %v", err_createCertMan)
-// 		return
-// 	}
-
-// 	// ExternalDNSApp := &appv1.Application{
-// 	// 	TypeMeta: v1.TypeMeta{
-// 	// 		Kind:       "Application",
-// 	// 		APIVersion: "argoproj.io/v1alpha1",
-// 	// 	},
-// 	// 	ObjectMeta: v1.ObjectMeta{
-// 	// 		Name:      "external-dns-app",
-// 	// 		Namespace: "argocd",
-// 	// 	},
-// 	// 	Spec: appv1.ApplicationSpec{
-// 	// 		Source: appv1.ApplicationSource{
-// 	// 			RepoURL: "https://github.com/jgwest/argocd-example-apps.git",
-// 	// 			Path:    "appset-examples/cluster-addon/prometheus-operator",
-// 	// 			Helm: &appv1.ApplicationSourceHelm{
-// 	// 				Version: "v2",
-// 	// 			},
-// 	// 		},
-// 	// 		Destination: appv1.ApplicationDestination{
-// 	// 			Namespace: "external-dns-app",
-// 	// 			Server:    "https://kubernetes.default.svc",
-// 	// 		},
-// 	// 		Project: "default",
-// 	// 	},
-// 	// }
-
-// 	// _, err_createDNS := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Create(context.TODO(), ExternalDNSApp, v1.CreateOptions{})
-// 	// if err_createDNS != nil {
-// 	// 	t.Errorf("error, %v", err_createDNS)
-// 	// 	return
-// 	// }
-
-// 	// Check if the Pod is getting refreshed or not
-// 	if reflect.DeepEqual(PodC, PodRestartcount(namespace)) != true {
-// 		t.Errorf("error, %v", "The Pods are getting refreshed!")
-// 		return
-// 	}
-
-// 	fmt.Print("\n\nPods Memory Resource after Creation:\n\n")
-// 	memoryPost := Get_pod_info(kubeconfig, master, namespace, podName)
-// 	PodInfoParse(memoryPost)
-
-// 	fmt.Print("\n\nDifference in the Pod Memory Usage (in Ki)\n\n")
-// 	delta := PodMemoryDiff(memoryInit, memoryPost)
-// 	PodInfoParse(delta)
-
-// 	// less use of generic sleep statements in automated tests
-// 	duration := time.Duration(10) * time.Second
-// 	time.Sleep(duration)
-
-// 	// To delete a application from the given (ex: argocd) namespace
-// 	err_delPrometheus := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Delete(context.TODO(), "prometheus-app", v1.DeleteOptions{})
-// 	if err_delPrometheus != nil {
-// 		t.Errorf("error, %v", err_delPrometheus)
-// 		return
-// 	}
-// 	// err_delExternalDNS := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Delete(context.TODO(), "external-dns-app", v1.DeleteOptions{})
-// 	// if err_delExternalDNS != nil {
-// 	// 	t.Errorf("error, %v", err_delExternalDNS)
-// 	// 	return
-// 	// }
-
-// 	// err_delCertMan := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Delete(context.TODO(), "cert-manager-app", v1.DeleteOptions{})
-// 	// if err_delCertMan != nil {
-// 	// 	t.Errorf("error, %v", err_delCertMan)
-// 	// 	return
-// 	// }
-// 	fmt.Println("Prometheus, External DNS and Cert Manager applications are created, deployed and deleted successfully!")
-// }
-
-func TestAllApplication(t *testing.T) {
-
-	// TEST 3: Deply Prometheus (heavy application) + 20 Guestbook Application (small application)
-
-	fmt.Print("Testing for Prometheus + 20 Guestbook Application\n")
-
-	fmt.Print("\n\nPods Memory Resource before creation:\n\n")
+	t.Log("\n\nPods Memory Resource before creation:\n\n")
 	// Intial Memory Information
-	memoryInit := Get_pod_info(kubeconfig, master, namespace, podName)
+	memoryInit := GetPodInfo(kubeconfig, master, namespace, podName)
 	PodInfoParse(memoryInit)
 
 	// Get Pod Restart Count
-	PodC := PodRestartcount(namespace)
+	PodC := PodRestartCount(namespace)
+
+	PrometheusApp := &appv1.Application{
+		TypeMeta: v1.TypeMeta{
+			Kind:       "Application",
+			APIVersion: "argoproj.io/v1alpha1",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "prometheus-app",
+			Namespace: "argocd",
+		},
+		Spec: appv1.ApplicationSpec{
+			Source: appv1.ApplicationSource{
+				RepoURL: "https://github.com/jgwest/argocd-example-apps.git",
+				Path:    "appset-examples/cluster-addon/prometheus-operator",
+				Helm: &appv1.ApplicationSourceHelm{
+					Version: "v2",
+				},
+			},
+			Destination: appv1.ApplicationDestination{
+				Namespace: "prometheus-app",
+				Server:    "https://kubernetes.default.svc",
+			},
+			Project: "default",
+		},
+	}
+
+	_, err_createPrometheus := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Create(context.TODO(), PrometheusApp, v1.CreateOptions{})
+	if err_createPrometheus != nil {
+		t.Errorf("error, %v", err_createPrometheus)
+		return
+	}
+
+	CertManagerApp := &appv1.Application{
+		TypeMeta: v1.TypeMeta{
+			Kind:       "Application",
+			APIVersion: "argoproj.io/v1alpha1",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "cert-manager-app",
+			Namespace: "argocd",
+		},
+		Spec: appv1.ApplicationSpec{
+			Source: appv1.ApplicationSource{
+				Path:    "cert-manager",
+				RepoURL: "https://github.com/samyak-jn/gitops-sample-apps.git",
+				Helm: &appv1.ApplicationSourceHelm{
+					Version: "v2",
+				},
+			},
+			Destination: appv1.ApplicationDestination{
+				Namespace: "cert-manager",
+				Server:    "https://kubernetes.default.svc",
+			},
+			Project: "default",
+		},
+	}
+
+	_, err_createCertMan := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Create(context.TODO(), CertManagerApp, v1.CreateOptions{})
+	if err_createCertMan != nil {
+		t.Errorf("error, %v", err_createCertMan)
+		return
+	}
+
+	ExternalDNSApp := &appv1.Application{
+		TypeMeta: v1.TypeMeta{
+			Kind:       "Application",
+			APIVersion: "argoproj.io/v1alpha1",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "external-dns-app",
+			Namespace: "argocd",
+		},
+		Spec: appv1.ApplicationSpec{
+			Source: appv1.ApplicationSource{
+				Path: "external-dns",
+				Helm: &appv1.ApplicationSourceHelm{
+					Version: "v2",
+				},
+				RepoURL: "https://github.com/samyak-jn/gitops-sample-apps.git",
+			},
+			Destination: appv1.ApplicationDestination{
+				Namespace: "external-dns",
+				Server:    "https://kubernetes.default.svc",
+			},
+			Project: "default",
+		},
+	}
+
+	_, err_createDNS := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Create(context.TODO(), ExternalDNSApp, v1.CreateOptions{})
+	if err_createDNS != nil {
+		t.Errorf("error, %v", err_createDNS)
+		return
+	}
+
+	// Check if the Pod is getting refreshed or not
+	if reflect.DeepEqual(PodC, PodRestartCount(namespace)) != true {
+		t.Errorf("error, %v", "The Pods are getting refreshed!")
+		return
+	}
+
+	t.Log("\n\nPods Memory Resource after Creation:\n\n")
+	memoryPost := GetPodInfo(kubeconfig, master, namespace, podName)
+	PodInfoParse(memoryPost)
+
+	t.Log("\n\nDifference in the Pod Memory Usage (in Ki)\n\n")
+	delta := PodMemoryDiff(memoryInit, memoryPost)
+	PodInfoParse(delta)
+
+	// less use of generic sleep statements in automated tests
+	duration := time.Duration(10) * time.Second
+	time.Sleep(duration)
+
+	// To delete a application from the given (ex: argocd) namespace
+	err_Prometheus := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Delete(context.TODO(), "prometheus-app", v1.DeleteOptions{})
+	if err_Prometheus != nil {
+		t.Errorf("error, %v", err_Prometheus)
+		return
+	}
+	err_ExternalDNS := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Delete(context.TODO(), "external-dns-app", v1.DeleteOptions{})
+	if err_ExternalDNS != nil {
+		t.Errorf("error, %v", err_ExternalDNS)
+		return
+	}
+
+	err_CertManager := GetE2EFixtureK8sClient().AppClientset.ArgoprojV1alpha1().Applications(namespace).Delete(context.TODO(), "cert-manager-app", v1.DeleteOptions{})
+	if err_CertManager != nil {
+		t.Errorf("error, %v", err_CertManager)
+		return
+	}
+	t.Log("Prometheus, External DNS and Cert Manager applications are created, deployed and deleted successfully!")
+}
+
+func TestAllApplication(t *testing.T) {
+
+	//	TEST 3: Deply Prometheus (heavy application) + 20 Guestbook Application (small application)
+
+	t.Log("Testing for Prometheus + 20 Guestbook Application\n")
+
+	t.Log("\n\nPods Memory Resource before creation:\n\n")
+	// Intial Memory Information
+	memoryInit := GetPodInfo(kubeconfig, master, namespace, podName)
+	PodInfoParse(memoryInit)
+
+	// Get Pod Restart Count
+	PodC := PodRestartCount(namespace)
 
 	PromethusApp := &appv1.Application{
 		TypeMeta: v1.TypeMeta{
@@ -305,7 +305,7 @@ func TestAllApplication(t *testing.T) {
 					RepoURL:        "https://github.com/argoproj/argocd-example-apps.git",
 				},
 				Destination: appv1.ApplicationDestination{
-					Namespace: "guestbook-" + fmt.Sprintf("%d", i),
+					Namespace: "guestbook",
 					Server:    "https://kubernetes.default.svc",
 				},
 				Project: "default",
@@ -320,16 +320,16 @@ func TestAllApplication(t *testing.T) {
 	}
 
 	// Check if the Pod is getting refreshed or not
-	if reflect.DeepEqual(PodC, PodRestartcount(namespace)) != true {
+	if reflect.DeepEqual(PodC, PodRestartCount(namespace)) != true {
 		t.Errorf("error, %v", "The Pods are getting refreshed!")
 		return
 	}
 
-	fmt.Print("\n\nPods Memory Resource after Creation:\n\n")
-	memoryPost := Get_pod_info(kubeconfig, master, namespace, podName)
+	t.Log("\n\nPods Memory Resource after Creation:\n\n")
+	memoryPost := GetPodInfo(kubeconfig, master, namespace, podName)
 	PodInfoParse(memoryPost)
 
-	fmt.Print("\n\nDifference in the Pod Memory Usage (in Ki)\n\n")
+	t.Log("\n\nDifference in the Pod Memory Usage (in Ki)\n\n")
 	delta := PodMemoryDiff(memoryInit, memoryPost)
 	PodInfoParse(delta)
 
@@ -352,5 +352,5 @@ func TestAllApplication(t *testing.T) {
 		t.Errorf("error, %v", err_del)
 		return
 	}
-	fmt.Println("Prometheus, along with 20 guestbook applications are created, deployed and deleted successfully!")
+	t.Log("Prometheus, along with 20 guestbook applications are created, deployed and deleted successfully!")
 }

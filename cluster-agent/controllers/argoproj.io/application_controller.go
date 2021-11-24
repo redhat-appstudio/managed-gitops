406e9b9e2568fa0a647e46d359e6bdddd5e69118
Samyak Jain <samyak.jn11@gmail.com>
2021-11-24 18:40:54 +0530

    Adds use case for updating the application CR in namespaces, and updating database a/c to GITOPS-1378
    Adds dbAppToApplicationCR method to create Applicaton in the namespace
    
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- cluster-agent/controllers/argoproj.io/application_controller.go
+++ cluster-agent/controllers/argoproj.io/application_controller.go
@@ -18,11 +18,15 @@ package argoprojio
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	appv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
-	db "github.com/redhat-appstudio/managed-gitops/backend-shared/config/db"
+	"github.com/redhat-appstudio/managed-gitops/backend-shared/config/db"
+	database "github.com/redhat-appstudio/managed-gitops/backend-shared/config/db"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -44,16 +48,87 @@ type ApplicationReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
+
 func (r *ApplicationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	contextLog := log.FromContext(ctx)
 
-	app := appv1.Application{}
-
-	if err := r.Get(ctx, req.NamespacedName, &app); err != nil {
-		contextLog.Error(err, "Unable to find Application: "+app.Name)
+	// creating databaseQueries object to fetch all the queries functionality
+	databaseQueries, err := database.NewUnsafePostgresDBQueries(true, false)
+	if err != nil {
 		return ctrl.Result{}, err
 	}
 
+	// retrieve the application CR from the namespace and store the value in app object
+	app := appv1.Application{}
+	if err := r.Get(ctx, req.NamespacedName, &app); err != nil {
+		contextLog.Error(err, "Unable to find Application: "+app.Name)
+
+		// Todo: to decide what will be Application_id from Namespace Application CR perspective
+		dbApplication := &database.Application{Application_id: string(app.UID)}
+
+		errGetApp := databaseQueries.UnsafeGetApplicationById(ctx, dbApplication)
+		// this proves that the the application CR is neither present in the namespace nor in the database
+		if errGetApp != nil {
+			return ctrl.Result{}, errGetApp
+		}
+
+		// Case 1: If nil, then Application CR is present in the database, hence create a new Application CR in the namespace
+		application, errConvert := dbAppToApplicationCR(dbApplication)
+		if errConvert != nil {
+			return ctrl.Result{}, errConvert
+		}
+		errAddToNamespace := r.Create(ctx, application, &client.CreateOptions{})
+		if errAddToNamespace != nil {
+			return ctrl.Result{}, errAddToNamespace
+		}
+
+		// Update the application state after application is pushed to Namespace
+		// if ApplicationState Row exists no change, else create!
+
+		dbApplicationState := &database.ApplicationState{Applicationstate_application_id: string(app.UID)}
+		errAppState := databaseQueries.UnsafeGetApplicationStateById(ctx, dbApplicationState)
+		if errAppState != nil {
+			dbApplicationState := &database.ApplicationState{
+				Applicationstate_application_id: dbApplication.Application_id,
+				Health:                          string(application.Status.Health.Status),
+				Sync_Status:                     string(application.Status.Sync.Status),
+			}
+
+			errCreateApplicationState := databaseQueries.UnsafeCreateApplicationState(ctx, dbApplicationState)
+			if errCreateApplicationState != nil {
+				return ctrl.Result{}, errCreateApplicationState
+			}
+		}
+
+	}
+	// verify if the Application_id is similar to the app.UID
+	retrievedApplication := &database.Application{Application_id: string((app.UID))}
+
+	// Case 2: If Application CR dosen't exist in database, remove from the namespace as well
+	err = databaseQueries.UnsafeGetApplicationById(ctx, retrievedApplication)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			errDel := r.Delete(ctx, &app, &client.DeleteAllOfOptions{})
+			if errDel != nil {
+				return ctrl.Result{}, fmt.Errorf("error, %v ", errDel)
+			}
+
+		}
+	}
+
+	// Case 3: If Application CR exists in database + namspace, compare!
+	specDetails, _ := json.Marshal(app.Spec)
+	if retrievedApplication.Spec_field != string(specDetails) {
+		newSpecErr := json.Unmarshal([]byte(retrievedApplication.Spec_field), &app.Spec)
+		if newSpecErr != nil {
+			return ctrl.Result{}, newSpecErr
+		}
+
+		errUpdate := r.Update(ctx, &app, &client.UpdateOptions{})
+		if errUpdate != nil {
+			return ctrl.Result{}, errUpdate
+		}
+	}
 	contextLog.Info("Application event seen in reconciler: " + app.Name)
 
 	return ctrl.Result{}, nil
@@ -142,4 +217,19 @@ func getGitOpsEngineInstanceForThisCluster(ctx context.Context, gitopsEngineInst
 
 	return fmt.Errorf("GitOpsEngineInstances were found, but no matches for namespace %v, for %v, owner of %v",
 		gitopsEngineNamespace.UID, gitopsEngineCluster.Gitopsenginecluster_id, ownerId)
+}
+
+func dbAppToApplicationCR(dbApp *database.Application) (*appv1.Application, error) {
+
+	newApplicationEntry := &appv1.Application{
+		//  Todo: should come from gitopsengineinstance!
+		ObjectMeta: v1.ObjectMeta{Name: dbApp.Name, Namespace: "argocd"},
+	}
+	// assigning spec
+	newSpecErr := json.Unmarshal([]byte(dbApp.Spec_field), &newApplicationEntry.Spec)
+	if newSpecErr != nil {
+		return nil, newSpecErr
+	}
+
+	return newApplicationEntry, nil
 }

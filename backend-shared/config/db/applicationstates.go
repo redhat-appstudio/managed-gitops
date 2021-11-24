b271ef161ea7e5288cc0f6459f5b8d19c4e20b70
Samyak Jain <samyak.jn11@gmail.com>
2021-11-24 18:34:15 +0530

    Add method for UnsafeCreateApplicationState and UnsafeUpdateApplicationState for the ApplicationState queries
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- backend-shared/config/db/applicationstates.go
+++ backend-shared/config/db/applicationstates.go
@@ -35,3 +35,66 @@ func (dbq *PostgreSQLDatabaseQueries) UncheckedDeleteApplicationStateById(ctx co
 
 	return deleteResult.RowsAffected(), nil
 }
+
+func (dbq *PostgreSQLDatabaseQueries) UnsafeCreateApplicationState(ctx context.Context, obj *ApplicationState) error {
+
+	if dbq.allowTestUuids {
+		if isEmpty(obj.Applicationstate_application_id) {
+			obj.Applicationstate_application_id = generateUuid()
+		}
+	} else {
+		if !isEmpty(obj.Applicationstate_application_id) {
+			return fmt.Errorf("primary key should be empty")
+		}
+
+		obj.Applicationstate_application_id = generateUuid()
+	}
+
+	if err := validateUnsafeQueryParamsNoPK(dbq); err != nil {
+		return err
+	}
+
+	if dbq.dbConnection == nil {
+		return fmt.Errorf("database connection is nil")
+	}
+	if !dbq.allowUnsafe {
+		return fmt.Errorf("unsafe operation is not allowed in this context")
+	}
+	if isEmpty(obj.Health) {
+		return fmt.Errorf("Application's initial health state should not be empty")
+	}
+	if isEmpty(obj.Sync_Status) {
+		return fmt.Errorf("Application's initial sync state should not be empty")
+	}
+	// inserting application object
+	result, err := dbq.dbConnection.Model(obj).Context(ctx).Insert()
+	if err != nil {
+		return fmt.Errorf("error on inserting application %v", err)
+	}
+
+	if result.RowsAffected() != 1 {
+		return fmt.Errorf("unexpected number of rows affected: %d", result.RowsAffected())
+	}
+	return nil
+}
+
+func (dbq *PostgreSQLDatabaseQueries) UnsafeUpdateApplicationState(ctx context.Context, obj *ApplicationState) error {
+	if dbq.dbConnection == nil {
+		return fmt.Errorf("database connection is nil")
+	}
+
+	if err := validateUnsafeQueryParamsNoPK(dbq); err != nil {
+		return err
+	}
+
+	result, err := dbq.dbConnection.Model(obj).Context(ctx).Update()
+	if err != nil {
+		return fmt.Errorf("error on updating application %v", err)
+	}
+
+	if result.RowsAffected() != 1 {
+		return fmt.Errorf("unexpected number of rows affected: %d", result.RowsAffected())
+	}
+
+	return nil
+}

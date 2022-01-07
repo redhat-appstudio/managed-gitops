605bc99c26553b26f35884fffe99e2184e13829e
Samyak Jain <samyak.jn11@gmail.com>
2022-01-07 19:02:22 +0530

    Add methods for creating a service account and fetch trhe bearer token for creating a client object
    Add UT for testing the CreateServiceAccount method
    
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

---
+++ backend-shared/hack/service_account.go
@@ -0,0 +1,175 @@
+package hack
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	apierr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubectl/pkg/scheme"
+	client "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	ArgoCDManagerServiceAccount     = "argocd-manager"
+	ArgoCDManagerClusterRole        = "argocd-manager-role"
+	ArgoCDManagerClusterRoleBinding = "argocd-manager-role-binding"
+)
+
+var (
+	hostname                          = "https://kubernetes.svc.default"
+	ArgoCDManagerNamespacePolicyRules = []rbacv1.PolicyRule{
+		{
+			APIGroups: []string{"*"},
+			Resources: []string{"*"},
+			Verbs:     []string{"*"},
+		},
+	}
+)
+
+func CreateServiceAccount(clientset kubernetes.Interface, serviceAccountName string, namespace string) error {
+
+	serviceAccount := corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "v1",
+			APIVersion: "ServiceAccount",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        serviceAccountName,
+			Namespace:   namespace,
+			ClusterName: "",
+		},
+	}
+	_, err := clientset.CoreV1().ServiceAccounts(namespace).Create(context.Background(), &serviceAccount, metav1.CreateOptions{})
+	if err != nil {
+		if !apierr.IsAlreadyExists(err) {
+			return fmt.Errorf("Failed to create service account %q in namespace %q: %v", serviceAccountName, namespace, err)
+		}
+		log.Fatalf("ServiceAccount %q already exists in namespace %q", serviceAccountName, namespace)
+		return nil
+	}
+	log.Fatalf("ServiceAccount %q created in namespace %q", serviceAccountName, namespace)
+	return nil
+}
+
+func installServiceAccount(clientset kubernetes.Interface, ns string, namespaces []string) (string, error) {
+	// ArgoCDManagerServiceAccount = "argocd-manager", is the name of the service account for managing a cluster
+
+	err := CreateServiceAccount(clientset, "argocd-manager", ns)
+	if err != nil {
+		return "", err
+	}
+	if len(namespaces) != 0 {
+		for _, namespace := range namespaces {
+			role := rbacv1.Role{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "rbac.authorization.k8s.io/v1",
+					Kind:       "Role",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: ArgoCDManagerClusterRole,
+				},
+				Rules: ArgoCDManagerNamespacePolicyRules,
+			}
+			_, err := clientset.RbacV1().Roles(namespace).Create(context.Background(), &role, metav1.CreateOptions{})
+			if err != nil {
+				if !apierr.IsAlreadyExists(err) {
+					_, err := clientset.RbacV1().Roles(namespace).Update(context.Background(), &role, metav1.UpdateOptions{})
+					if err != nil {
+						return "", err
+					}
+				}
+			}
+
+			roleBinding := rbacv1.RoleBinding{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "rbac.authorization.k8s.io/v1",
+					Kind:       "RoleBinding",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: ArgoCDManagerClusterRoleBinding,
+				},
+				RoleRef: rbacv1.RoleRef{
+					APIGroup: "rbac.authorization.k8s.io",
+					Kind:     "Role",
+					Name:     ArgoCDManagerClusterRole,
+				},
+				Subjects: []rbacv1.Subject{{
+					Kind:      rbacv1.ServiceAccountKind,
+					Name:      ArgoCDManagerServiceAccount,
+					Namespace: ns,
+				}},
+			}
+			_, err = clientset.RbacV1().RoleBindings(namespace).Create(context.Background(), &roleBinding, metav1.CreateOptions{})
+			if err != nil {
+				if !apierr.IsAlreadyExists(err) {
+					_, err := clientset.RbacV1().RoleBindings(namespace).Update(context.Background(), &roleBinding, metav1.UpdateOptions{})
+					if err != nil {
+						return "", err
+					}
+				}
+			}
+		}
+	}
+	return GetServiceAccountBearerToken(clientset, ns, ArgoCDManagerServiceAccount)
+}
+
+// GetServiceAccountBearerToken will attempt to get the provided service account until it
+// exists, iterate the secrets associated with it looking for one of type
+// kubernetes.io/service-account-token, and return it's token if found.
+func GetServiceAccountBearerToken(clientset kubernetes.Interface, ns string, sa string) (string, error) {
+	var serviceAccount *corev1.ServiceAccount
+	var secret *corev1.Secret
+	var err error
+	err = wait.Poll(500*time.Millisecond, 30*time.Second, func() (bool, error) {
+		serviceAccount, err = clientset.CoreV1().ServiceAccounts(ns).Get(context.Background(), sa, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		// Scan all secrets looking for one of the correct type:
+		for _, oRef := range serviceAccount.Secrets {
+			var getErr error
+			secret, err = clientset.CoreV1().Secrets(ns).Get(context.Background(), oRef.Name, metav1.GetOptions{})
+			if err != nil {
+				return false, fmt.Errorf("Failed to retrieve secret %q: %v", oRef.Name, getErr)
+			}
+			if secret.Type == corev1.SecretTypeServiceAccountToken {
+				return true, nil
+			}
+		}
+		return false, nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("Failed to wait for service account secret: %v", err)
+	}
+	token, ok := secret.Data["token"]
+	if !ok {
+		return "", fmt.Errorf("Secret %q for service account %q did not have a token", secret.Name, serviceAccount)
+	}
+	return string(token), nil
+}
+
+func generateClientFromClusterServiceAccount(hostname string, bearerToken string) client.Client {
+	config := &rest.Config{
+		Host: hostname,
+		// what is the best way to fetch the username and password - bearerToken is alternative to username
+		// Username:    "",
+		// Password:    "",
+		BearerToken: bearerToken,
+	}
+	// what should go under clientOptions!
+	clientObj, err := client.New(config, client.Options{Scheme: scheme.Scheme})
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return clientObj
+}

dbb955246fecd611d3ff5630d7d08cfdddf97626
Akshay Bhansali <abhansal@redhat.com>
2022-03-15 11:52:19 +0530

    go parser to check if schema fields and constants are in sync

---
+++ backend-shared/hack/run-ci-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+set -o errexit
+set -o nounset
+set -o pipefail
+
+BACKEND_SHARED_DIR=$GITHUB_WORKSPACE/backend-shared
+cd ${BACKEND_SHARED_DIR}
+go build -o ./hack/dist/ci-check ./hack/ci-check
+./hack/dist/ci-check

6c8ff18a658c32a1063be384037ec78022aaee1b
Akshay Bhansali <abhansal@redhat.com>
2022-03-15 10:22:46 +0530

    execute binary

--- backend-shared/hack/run-ci-check.sh
+++ backend-shared/hack/run-ci-check.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-BACKEND_SHARED_DIR=${GITHUB_WORKSPACE}/backend-shared
+BACKEND_SHARED_DIR=$GITHUB_WORKSPACE/backend-shared
 
 go build -o ${BACKEND_SHARED_DIR}/hack/dist/ci-check ${BACKEND_SHARED_DIR}/hack/ci-check
-${BACKEND_SHARED_DIR}/dist/ci-check
+./${BACKEND_SHARED_DIR}/dist/ci-check

fb3f6839cd679951b34b7cd3a67008f41e8c653d
Akshay Bhansali <abhansal@redhat.com>
2022-03-16 16:35:01 +0530

    code refactoring and changes

--- backend-shared/hack/db-schema-sync-check/main.go
+++ backend-shared/hack/db-schema-sync-check/main.go
@@ -8,58 +8,41 @@ import (
 )
 
 const (
-	DbSchemaRelativeFileLocation         = "../db-schema.sql"
-	DbFieldConstantsRelativeFileLocation = "./config/db/db_field_constants.go"
+	DBSchemaRelativeFileLocation         = "../db-schema.sql"
+	DBFieldConstantsRelativeFileLocation = "./config/db/db_field_constants.go"
 )
 
 func main() {
-
-	fieldToSize := parseDbSchema()
-	fieldConstantToSize := parseDbConstants()
+	fieldToSize := parseDbSchema(DBSchemaRelativeFileLocation)
+	fieldConstantToSize := parseDbConstants(DBFieldConstantsRelativeFileLocation)
 	checkIfSchemaInSyncWithConstants(fieldConstantToSize, fieldToSize)
-
 }
 
 func checkIfSchemaInSyncWithConstants(fieldConstantToSize map[string]string, fieldToSize map[string]string) {
-
 	for fieldName, fieldSize := range fieldConstantToSize {
 		if _, ok := fieldToSize[fieldName]; !ok {
-			err := fmt.Errorf("field %s not present in the schema", fieldName)
-			fmt.Println(err)
-			os.Exit(1)
+			exitWithError(fmt.Errorf("field %s not present in the schema", fieldName))
 		}
 
 		if fieldToSize[fieldName] != fieldSize {
-			err := fmt.Errorf("sizes for the field %s are not in sync", fieldName)
-			fmt.Println(err)
-			os.Exit(1)
+			exitWithError(fmt.Errorf("sizes for the field %s are not in sync", fieldName))
 		}
-
 	}
 
 	for fieldName, fieldSize := range fieldToSize {
-
 		if _, ok := fieldConstantToSize[fieldName]; !ok {
-
-			err := fmt.Errorf("field %s not present as a constant in the db_field_constants.go file", fieldName)
-			fmt.Println(err)
-			os.Exit(1)
+			exitWithError(fmt.Errorf("field %s not present as a constant in the db_field_constants.go file", fieldName))
 		}
 
 		if fieldConstantToSize[fieldName] != fieldSize {
-
-			err := fmt.Errorf("sizes for the field %s are not in sync", fieldName)
-			fmt.Println(err)
-			os.Exit(1)
+			exitWithError(fmt.Errorf("sizes for the field %s are not in sync", fieldName))
 		}
 	}
 }
 
-func parseDbSchema() map[string]string {
-
+func parseDbSchema(DBSchemaRelativeFileLocation string) map[string]string {
 	fieldToSize := make(map[string]string)
-	dbSchemaFile, err := os.ReadFile(DbSchemaRelativeFileLocation)
-
+	dbSchemaFile, err := os.ReadFile(DBSchemaRelativeFileLocation)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -69,25 +52,22 @@ func parseDbSchema() map[string]string {
 
 	i := 0
 	for i < len(dbSchema) {
-
-		//process the fields of each table individually by identifying the beginning of the creation
-		//of a table with the substring "CREATE TABLE" and the end of a table with ");"
+		// process the fields of each table individually by identifying the beginning of the creation
+		// of a table with the substring "CREATE TABLE" and the end of a table with ");"
 		if strings.Contains(dbSchema[i], "CREATE TABLE") {
-
 			tableName := strings.Split(dbSchema[i], " ")[2]
-			//process the current table till a ");" is encountered which marks the end of the table's definition
+			// process the current table till a ");" is encountered which marks the end of the table's definition
 			for !(strings.Contains(dbSchema[i], ");")) {
 
-				//check if the current string contains the type as VARCHAR, and extract the name of the field and size of the field
-				//while omitting comments
+				// check if the current string contains the type as VARCHAR, and extract the name of the field and size of the field
+				// while omitting comments
 				if strings.Contains(dbSchema[i], "VARCHAR") && !(strings.Contains(dbSchema[i], "--")) {
-
 					spaces := regexp.MustCompile(`\s+`)
-					//remove all leading white spaces
+					// remove all leading white spaces
 					fieldName := spaces.ReplaceAllString(strings.Split(dbSchema[i], " ")[0], "")
 					fieldNameInCamelCase := convertSnakeCaseToCamelCase(fieldName)
 					currentField := strings.ReplaceAll(dbSchema[i], " ", "")
-					//extract size of the VARCHAR field as a substring
+					// extract size of the VARCHAR field as a substring
 					StartIndexOfSize := strings.Index(currentField, "(")
 					endIndexOfSize := strings.Index(currentField, ")")
 					size := currentField[StartIndexOfSize+1 : endIndexOfSize]
@@ -99,31 +79,27 @@ func parseDbSchema() map[string]string {
 		}
 		i++
 	}
+
 	return fieldToSize
 }
 
-func parseDbConstants() map[string]string {
-
+func parseDbConstants(DBFieldConstantsRelativeFileLocation string) map[string]string {
 	fieldConstantToSize := make(map[string]string)
-	dbFieldConstantsFile, err := os.ReadFile(DbFieldConstantsRelativeFileLocation)
-
+	dbFieldConstantsFile, err := os.ReadFile(DBFieldConstantsRelativeFileLocation)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		exitWithError(err)
 	}
 
 	dbFieldConstants := strings.Split(string(dbFieldConstantsFile), "\n")
 
 	i := 0
 	for !(strings.Contains(dbFieldConstants[i], "const")) {
-
 		i++
 	}
 	i++
 	for !(strings.Contains(dbFieldConstants[i], ")")) {
-
 		spaces := regexp.MustCompile(`\s+`)
-		//remove all white spaces for the current field
+		// remove all white spaces for the current field
 		dbFieldConstants[i] = spaces.ReplaceAllString(dbFieldConstants[i], "")
 		fieldDetails := strings.Split(dbFieldConstants[i], "=")
 		fieldName := fieldDetails[0]
@@ -131,11 +107,11 @@ func parseDbConstants() map[string]string {
 		fieldConstantToSize[fieldName] = fieldSize
 		i++
 	}
+
 	return fieldConstantToSize
 }
 
 func convertSnakeCaseToCamelCase(fieldName string) string {
-
 	splitFieldName := strings.Split(fieldName, "_")
 	var fieldNameInCamelCase string
 
@@ -144,4 +120,9 @@ func convertSnakeCaseToCamelCase(fieldName string) string {
 	}
 
 	return fieldNameInCamelCase
+}
+
+func exitWithError(err error) {
+	fmt.Println(err)
+	os.Exit(1)
 }

a0df3cd8b030508a5ee995d36db347b4a12878ae
Jonathan West <jonwest@redhat.com>
2022-04-04 17:31:39 -0400

    Refactor type name, increase slow test threshold, switch from panic to return

--- backend-shared/util/util_suite_test.go
+++ backend-shared/util/util_suite_test.go
@@ -2,12 +2,16 @@ package util
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
 	. "github.com/onsi/gomega"
 )
 
 func TestUtil(t *testing.T) {
+	config.DefaultReporterConfig.SlowSpecThreshold = time.Hour.Seconds() * 30
+
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "TaskRetryLoop Suite")
 }

1cf3ab4b9c56cd7bab9aade7b7186400c78a1f9d
Jonathan West <jonwest@redhat.com>
2022-04-07 12:52:33 -0400

    Add additional DB comments and cleanup

--- backend-shared/config/db/deploymenttoapplicationmapping_test.go
+++ backend-shared/config/db/deploymenttoapplicationmapping_test.go
@@ -43,7 +43,7 @@ func TestCreateandDeleteDeploymentToApplicationMapping(t *testing.T) {
 		Application_id:                        application.Application_id,
 		DeploymentName:                        "test-deployment",
 		DeploymentNamespace:                   "test-namespace",
-		WorkspaceUID:                          "demo-workspace",
+		NamespaceUID:                          "demo-workspace",
 	}
 
 	err = dbq.CreateDeploymentToApplicationMapping(ctx, deploymentToApplicationMapping)
@@ -103,7 +103,7 @@ func TestAllListDeploymentToApplicationMapping(t *testing.T) {
 		Application_id:                        application.Application_id,
 		DeploymentName:                        "test-deployment",
 		DeploymentNamespace:                   "test-namespace",
-		WorkspaceUID:                          "demo-workspace",
+		NamespaceUID:                          "demo-workspace",
 	}
 
 	err = dbq.CreateDeploymentToApplicationMapping(ctx, deploymentToApplicationMapping)
@@ -122,7 +122,7 @@ func TestAllListDeploymentToApplicationMapping(t *testing.T) {
 	assert.True(t, len(dbResults) == 1)
 	assert.Equal(t, dbResults[0], *deploymentToApplicationMapping)
 
-	err = dbq.ListDeploymentToApplicationMappingByNamespaceAndName(ctx, deploymentToApplicationMapping.DeploymentName, deploymentToApplicationMapping.DeploymentNamespace, deploymentToApplicationMapping.WorkspaceUID, &dbResults)
+	err = dbq.ListDeploymentToApplicationMappingByNamespaceAndName(ctx, deploymentToApplicationMapping.DeploymentName, deploymentToApplicationMapping.DeploymentNamespace, deploymentToApplicationMapping.NamespaceUID, &dbResults)
 	if !assert.NoError(t, err) {
 		return
 	}

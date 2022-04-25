e1435cce407bcb0d0aca668465b9c09b753b4295
Jonathan West <jonwest@redhat.com>
2022-04-25 11:56:42 -0400

    Switch argocd.sh to allow namespace customization, a new default namespace, and a new default port

--- argocd.sh
+++ argocd.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+ARGO_CD_VERSION="${ARGO_CD_VERSION:-stable}"
+ARGO_CD_NAMESPACE="${ARGO_CD_NAMESPACE:-gitops-service-argocd}"
+
 # This script installs ArgoCD vanilla according to https://argo-cd.readthedocs.io/en/stable/getting_started/
 
 # Checks if a binary is present on the local system
@@ -16,15 +19,15 @@ exit_if_binary_not_installed() {
 if [ "$1" = "install" ]; then
     exit_if_binary_not_installed "kubectl"
 
-    if kubectl -n argocd get pods | grep argocd-server | grep '1/1' | grep 'Running' &>/dev/null; then
+    if kubectl -n $ARGO_CD_NAMESPACE get pods | grep argocd-server | grep '1/1' | grep 'Running' &>/dev/null; then
         echo "ArgoCD is already running..."
         echo "Skipping ArgoCD setup."
         exit 1
     fi
 
     # Apply the argo-cd-route manifest
-    kubectl create namespace argocd || true
-    kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+    kubectl create namespace $ARGO_CD_NAMESPACE || true
+    kubectl apply -n $ARGO_CD_NAMESPACE -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGO_CD_VERSION/manifests/install.yaml
 
     # Get the secret
     counter=0
@@ -52,12 +55,12 @@ if [ "$1" = "install" ]; then
     echo " * argocd-server Pod is running."
 
     # Port forward the ArgoCD service locally, so we can access it
-    kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+    kubectl port-forward svc/argocd-server -n $ARGO_CD_NAMESPACE 18080:443 &
     KUBE_PID=$!
 
-    # Checks if 8080 is occupied
+    # Checks if 18080 is occupied
     counter=0
-    until lsof -i:8080 | grep LISTEN; do
+    until lsof -i:18080 | grep LISTEN; do
         sleep 1
         if [ "$counter" -gt 10 ]; then
             echo ".. retry $counter ..."
@@ -81,7 +84,7 @@ if [ "$1" = "install" ]; then
         echo " * Port-forwarding has been successfully stopped"
     fi
 
-    if lsof -i:8080 | grep LISTEN; then
+    if lsof -i:18080 | grep LISTEN; then
         echo " * Port forwarding is still active for some reason. Investigate further ..."
     fi
 
@@ -91,8 +94,8 @@ if [ "$1" = "install" ]; then
     echo "| To access the ArgoCD Web UI |"
     echo " ------------------------------"
     echo
-    echo "  - Run:            kubectl port-forward svc/argocd-server -n argocd 8080:443 &"
-    echo "  - Credentials:    HOST=localhost:8080  USERNAME=admin  PASSWORD=$ARGOCD_PASSWORD"
+    echo "  - Run:            kubectl port-forward svc/argocd-server -n argocd 18080:443 &"
+    echo "  - Credentials:    HOST=localhost:18080  USERNAME=admin  PASSWORD=$ARGOCD_PASSWORD"
     echo
     exit 0
 fi
@@ -100,5 +103,5 @@ fi
 # Remove 'ArgoCD Web UI' from your Kubernetes cluster
 if [ "$1" = "remove" ]; then
     exit_if_binary_not_installed "kubectl"
-    kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+    kubectl delete -n $ARGO_CD_NAMESPACE -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGO_CD_VERSION/manifests/install.yaml
 fi

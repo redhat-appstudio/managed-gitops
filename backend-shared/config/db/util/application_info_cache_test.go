cc9ade56a6331ed45cc125288ecaf416c393d102
Raghavi Shirur <raghaviss11@gmail.com>
2022-05-17 18:51:20 +0530

    fix lint issue for application_infor_cache_test.go

--- backend-shared/config/db/util/application_info_cache_test.go
+++ backend-shared/config/db/util/application_info_cache_test.go
@@ -165,7 +165,7 @@ var _ = Describe("application_info_cache Test", func() {
 
 			//testId doesn't exist, the test should report an error
 			app, valuefromCache, errGet := asc.GetApplicationById(ctx, testId)
-			Expect(err).To(BeNil())
+			Expect(errGet).To(BeNil())
 			// since no entry in the valuefromcache should be false, appState should be empty
 			Expect(valuefromCache).To(BeFalse())
 			Expect(app).To(Equal(db.Application{}))

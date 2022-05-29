bcaa5bfd16bb5f9d4565595d46020daec96639ea
Panagiotis Georgiadis <pgeorgia@redhat.com>
2022-05-29 04:08:39 +0200

    Add Delete and Get

--- backend-shared/config/db/repo_cred.go
+++ backend-shared/config/db/repo_cred.go
@@ -8,14 +8,12 @@ import (
 
 var (
 	errCreateRepositoryCredentials = errors.New("cannot insert repository credentials")
-	errRowsAffected                = errors.New("unexpected number of rows affected")
+	errDeleteRepositoryCredentials = errors.New("cannot delete repository credentials")
+	errGetRepositoryCredentials    = errors.New("cannot find repository credentials")
+	errRowsAffected                = errors.New("unexpected number of affected rows")
 )
 
 func (dbq *PostgreSQLDatabaseQueries) CreateRepositoryCredentials(ctx context.Context, obj *RepositoryCredentials) error {
-	// TODO: See similar tests and figure out what else is needed
-	// TODO: If it actually is needed here and the tests didn't catch it
-	// then it is either not required OR test is missing for it.
-
 	result, err := dbq.dbConnection.Model(obj).Context(ctx).Insert()
 	if err != nil {
 		return fmt.Errorf("%v: %w", errCreateRepositoryCredentials, err)
@@ -26,4 +24,43 @@ func (dbq *PostgreSQLDatabaseQueries) CreateRepositoryCredentials(ctx context.Co
 	}
 
 	return err
+}
+
+func (dbq *PostgreSQLDatabaseQueries) DeleteRepositoryCredentialsByID(ctx context.Context, id string) error {
+	if err := validateQueryParams(id, dbq); err != nil {
+		return err
+	}
+
+	repoCred := &RepositoryCredentials{
+		PrimaryKeyID: id,
+	}
+
+	// Delete the database row, if PK is valid.
+	result, err := dbq.dbConnection.Model(repoCred).WherePK().Context(ctx).Delete()
+	if err != nil {
+		return fmt.Errorf("%v: %w", errDeleteRepositoryCredentials, err)
+	}
+
+	if result.RowsAffected() != 1 {
+		return fmt.Errorf("%w: %d", errRowsAffected, result.RowsAffected())
+	}
+
+	return nil
+}
+
+func (dbq *PostgreSQLDatabaseQueries) GetRepositoryCredentialsByID(ctx context.Context, id string) (repoCred RepositoryCredentials, err error) {
+	if err = validateQueryParams(id, dbq); err != nil {
+		return repoCred, err
+	}
+
+	repoCred = RepositoryCredentials{
+		PrimaryKeyID: id,
+	}
+
+	err = dbq.dbConnection.Model(&repoCred).WherePK().Context(ctx).Select()
+	if err != nil {
+		return repoCred, fmt.Errorf("%v: %w", errGetRepositoryCredentials, err)
+	}
+
+	return repoCred, nil
 }
--- backend-shared/config/db/repo_cred_test.go
+++ backend-shared/config/db/repo_cred_test.go
@@ -2,27 +2,36 @@ package db_test
 
 import (
 	"context"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/redhat-appstudio/managed-gitops/backend-shared/config/db"
 )
 
 var _ = Describe("RepositoryCredentials Tests", func() {
-	Context("Given there are DB functions for RepositoryCredentials", func() {
-		It("should all execute without problems", func() {
-			err := db.SetupForTestingDBGinkgo()
+	var (
+		err                  error
+		ctx                  context.Context
+		clusterUser          *db.ClusterUser
+		clusterCredentials   db.ClusterCredentials
+		gitopsEngineCluster  db.GitopsEngineCluster
+		gitopsEngineInstance db.GitopsEngineInstance
+		dbq                  db.AllDatabaseQueries
+	)
+
+	When("When ClusterUser, ClusterCredentials, GitopsEngine and GitopsInstance exist", func() {
+		BeforeEach(func() {
+			// Connect to the database (the connection closes at AfterEach)
+			err = db.SetupForTestingDBGinkgo()
 			Expect(err).To(BeNil())
 
-			dbq, err := db.NewUnsafePostgresDBQueries(true, true)
+			dbq, err = db.NewUnsafePostgresDBQueries(true, true)
 			Expect(err).To(BeNil())
-			defer dbq.CloseDatabase()
 
-			ctx := context.Background()
+			ctx = context.Background()
 
 			// Satisfy the foreign key constraint 'fk_clusteruser_id'
 			// aka: ClusterUser.Clusteruser_id) required for 'repo_cred_user_id'
-			clusterUser := &db.ClusterUser{
+			clusterUser = &db.ClusterUser{
 				Clusteruser_id: "fake-repocred-user-id",
 				User_name:      "fake-repocred-user",
 			}
@@ -31,7 +40,7 @@ var _ = Describe("RepositoryCredentials Tests", func() {
 
 			// Satisfy the foreign key constraint 'fk_gitopsengineinstance_id'
 			// aka: GitOpsEngineInstance.Gitopsengineinstance_id) required for 'repo_cred_gitopsengineinstance_id'
-			clusterCredentials := db.ClusterCredentials{
+			clusterCredentials = db.ClusterCredentials{
 				Clustercredentials_cred_id:  "fake-repocred-Clustercredentials_cred_id",
 				Host:                        "fake-repocred-host",
 				Kube_config:                 "fake-repocred-kube-config",
@@ -43,7 +52,7 @@ var _ = Describe("RepositoryCredentials Tests", func() {
 			err = dbq.CreateClusterCredentials(ctx, &clusterCredentials)
 			Expect(err).To(BeNil())
 
-			gitopsEngineCluster := db.GitopsEngineCluster{
+			gitopsEngineCluster = db.GitopsEngineCluster{
 				Gitopsenginecluster_id: "fake-repocred-Gitopsenginecluster_id",
 				Clustercredentials_id:  clusterCredentials.Clustercredentials_cred_id,
 			}
@@ -51,7 +60,7 @@ var _ = Describe("RepositoryCredentials Tests", func() {
 			err = dbq.CreateGitopsEngineCluster(ctx, &gitopsEngineCluster)
 			Expect(err).To(BeNil())
 
-			gitopsEngineInstance := db.GitopsEngineInstance{
+			gitopsEngineInstance = db.GitopsEngineInstance{
 				Gitopsengineinstance_id: "fake-repocred-Gitopsengineinstance_id",
 				Namespace_name:          "fake-repocred-Namespace_name",
 				Namespace_uid:           "fake-repocred-Namespace_uid",
@@ -60,11 +69,30 @@ var _ = Describe("RepositoryCredentials Tests", func() {
 
 			err = dbq.CreateGitopsEngineInstance(ctx, &gitopsEngineInstance)
 			Expect(err).To(BeNil())
+		})
+		AfterEach(func() {
+			// Delete Cluster User, Cluster Credentials, Gitops Engine Cluster, Gitops Engine Instance.
+			_, err = dbq.DeleteGitopsEngineInstanceById(ctx, gitopsEngineInstance.Gitopsengineinstance_id)
+			Expect(err).To(BeNil())
 
-			// Create a fake RepositoryCredentials
+			_, err = dbq.DeleteGitopsEngineClusterById(ctx, gitopsEngineCluster.Gitopsenginecluster_id)
+			Expect(err).To(BeNil())
+
+			_, err = dbq.DeleteClusterCredentialsById(ctx, clusterCredentials.Clustercredentials_cred_id)
+			Expect(err).To(BeNil())
+
+			_, err = dbq.DeleteClusterUserById(ctx, clusterUser.Clusteruser_id)
+			Expect(err).To(BeNil())
+
+			dbq.CloseDatabase() // Close the database connection.
+		})
+
+		It("it should create and delete RepositoryCredentials", func() {
+
+			// Create a RepositoryCredentials object.
 			gitopsRepositoryCredentials := db.RepositoryCredentials{
 				PrimaryKeyID:    "fake-repo-cred-id",
-				UserID:          clusterUser.Clusteruser_id, // contrain 'fk_clusteruser_id'
+				UserID:          clusterUser.Clusteruser_id, // constrain 'fk_clusteruser_id'
 				PrivateURL:      "https://fake-private-url",
 				AuthUsername:    "fake-auth-username",
 				AuthPassword:    "fake-auth-password",
@@ -74,7 +102,17 @@ var _ = Describe("RepositoryCredentials Tests", func() {
 				SeqID:           0,
 			}
 
+			// Insert the RepositoryCredentials to the database.
 			err = dbq.CreateRepositoryCredentials(ctx, &gitopsRepositoryCredentials)
+			Expect(err).To(BeNil())
+
+			// Get the RepositoryCredentials from the database.
+			fetch, err := dbq.GetRepositoryCredentialsByID(ctx, gitopsRepositoryCredentials.PrimaryKeyID)
+			Expect(err).To(BeNil())
+			Expect(fetch).Should(Equal(gitopsRepositoryCredentials))
+
+			// Delete the RepositoryCredentials from the database.
+			err = dbq.DeleteRepositoryCredentialsByID(ctx, gitopsRepositoryCredentials.PrimaryKeyID)
 			Expect(err).To(BeNil())
 
 		})

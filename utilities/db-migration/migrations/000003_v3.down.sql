ea9e12ebf93429f27b0e04cf169b9fb04c3e323d
Panagiotis Georgiadis <pgeorgia@redhat.com>
2022-06-17 10:21:51 +0200

    Migration modifications

--- utilities/db-migration/migrations/000003_v3.down.sql
+++ utilities/db-migration/migrations/000003_v3.down.sql
@@ -1,19 +1,2 @@
-DROP TABLE public.repositorycredentials (
-    repositorycredentials_id character varying(48) NOT NULL,
-    repo_cred_user_id character varying(48) NOT NULL,
-    repo_cred_url character varying(512) NOT NULL,
-    repo_cred_user character varying(256),
-    repo_cred_pass character varying(1024),
-    repo_cred_ssh character varying(1024),
-    repo_cred_secret character varying(48) NOT NULL,
-    repo_cred_engine_id character varying(48) NOT NULL,
-    seq_id integer NOT NULL
-);
-
-DROP SEQUENCE public.repositorycredentials_seq_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+DROP TABLE repositorycredentials;
+DROP SEQUENCE repositorycredentials_seq_id_seq;
--- utilities/db-migration/migrations/000003_v3.up.sql
+++ utilities/db-migration/migrations/000003_v3.up.sql
@@ -1,38 +1,34 @@
-CREATE TABLE public.repositorycredentials (
-    repositorycredentials_id character varying(48) NOT NULL,
-    repo_cred_user_id character varying(48) NOT NULL,
-    repo_cred_url character varying(512) NOT NULL,
-    repo_cred_user character varying(256),
-    repo_cred_pass character varying(1024),
-    repo_cred_ssh character varying(1024),
-    repo_cred_secret character varying(48) NOT NULL,
-    repo_cred_engine_id character varying(48) NOT NULL,
-    seq_id integer NOT NULL
+CREATE TABLE RepositoryCredentials (
+
+    -- Primary Key, that is an auto-generated UID
+    repositorycredentials_id VARCHAR ( 48 ) NOT NULL UNIQUE PRIMARY KEY,
+
+    -- User of GitOps service that wants to use a private repository
+    -- Foreign key to: ClusterUser.Clusteruser_id
+    repo_cred_user_id VARCHAR (48) NOT NULL,
+    CONSTRAINT fk_clusteruser_id FOREIGN KEY (repo_cred_user_id) REFERENCES ClusterUser(clusteruser_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+    -- URL of the Git repository (example: https://github.com/my-org/my-repo)
+    repo_cred_url VARCHAR (512) NOT NULL,
+
+    -- Authorized username login for accessing the private Git repo
+    repo_cred_user VARCHAR (256),
+
+    -- Authorized password login for accessing the private Git repo
+    repo_cred_pass VARCHAR (1024),
+
+    -- Alternative authentication method using an authorized private SSH key
+    repo_cred_ssh VARCHAR (1024),
+
+    -- The name of the Secret resource in the Argo CD Repository, in the GitOps Engine instance
+    repo_cred_secret VARCHAR(48) NOT NULL,
+
+    -- The internal RedHat Managed cluster where the GitOps Engine (e.g. ArgoCD) is running
+    -- NOTE: It is expected the 'repo_cred_secret' to be stored there as well.
+    -- Foreign key to: GitopsEngineInstance.Gitopsengineinstance_id
+    repo_cred_engine_id VARCHAR(48) NOT NULL,
+    CONSTRAINT fk_gitopsengineinstance_id FOREIGN KEY (repo_cred_engine_id) REFERENCES GitopsEngineInstance(gitopsengineinstance_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+    seq_id serial
+
 );
-
-ALTER TABLE public.repositorycredentials OWNER TO postgres;
-
-CREATE SEQUENCE public.repositorycredentials_seq_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER TABLE public.repositorycredentials_seq_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE public.repositorycredentials_seq_id_seq OWNED BY public.repositorycredentials.seq_id;
-
-ALTER TABLE ONLY public.repositorycredentials ALTER COLUMN seq_id SET DEFAULT nextval('public.repositorycredentials_seq_id_seq'::regclass);
-
-ALTER TABLE ONLY public.repositorycredentials
-    ADD CONSTRAINT repositorycredentials_pkey PRIMARY KEY (repositorycredentials_id);
-
-ALTER TABLE ONLY public.repositorycredentials
-    ADD CONSTRAINT fk_clusteruser_id FOREIGN KEY (repo_cred_user_id) REFERENCES public.clusteruser(clusteruser_id);
-
-ALTER TABLE ONLY public.repositorycredentials
-    ADD CONSTRAINT fk_gitopsengineinstance_id FOREIGN KEY (repo_cred_engine_id) REFERENCES public.gitopsengineinstance(gitopsengineinstance_id);
-
-

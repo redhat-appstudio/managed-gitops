69f681aa5c06b0e06572400a4b2a7f733f12d386
Chetan Banavikalmutt <chetanrns1997@gmail.com>
2022-06-21 14:59:37 +0530

    Generalize and unmarshall to get raw Application strings

--- backend/eventloop/application_event_loop/application_event_runner_deployments_test.go
+++ backend/eventloop/application_event_loop/application_event_runner_deployments_test.go
@@ -3,57 +3,12 @@ package application_event_loop
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/redhat-appstudio/managed-gitops/backend/util/fauxargocd"
+	"gopkg.in/yaml.v2"
 )
 
 var _ = Describe("Application Event Runner Deployments", func() {
 	Context("createSpecField() should generate a valid argocd Application", func() {
-
-		const (
-			validApplication = `fauxtypemeta:
-  kind: Application
-  apiversion: argoproj.io/v1alpha1
-fauxobjectmeta:
-  name: sample-depl
-  namespace: kcp-workspace
-spec:
-  source:
-    repourl: https://github.com/test/test
-    path: environments/prod
-    targetrevision: ""
-  destination:
-    server: ""
-    namespace: prod
-    name: in-cluster
-  project: default
-  syncpolicy: null
-`
-
-			validApplicationWithAutomatedSync = `fauxtypemeta:
-  kind: Application
-  apiversion: argoproj.io/v1alpha1
-fauxobjectmeta:
-  name: sample-depl
-  namespace: kcp-workspace
-spec:
-  source:
-    repourl: https://github.com/test/test
-    path: environments/prod
-    targetrevision: ""
-  destination:
-    server: ""
-    namespace: prod
-    name: in-cluster
-  project: default
-  syncpolicy:
-    automated:
-      prune: true
-      selfheal: true
-      allowempty: true
-    syncoptions: []
-    retry: null
-`
-		)
-
 		getfakeArgoCDSpecInput := func(automated, unsanitized bool) argoCDSpecInput {
 			input := argoCDSpecInput{
 				crName:               "sample-depl",
@@ -72,25 +27,67 @@ spec:
 			return input
 		}
 
+		getValidApplication := func(automated bool) string {
+			input := getfakeArgoCDSpecInput(automated, false)
+			application := fauxargocd.FauxApplication{
+				FauxTypeMeta: fauxargocd.FauxTypeMeta{
+					Kind:       "Application",
+					APIVersion: "argoproj.io/v1alpha1",
+				},
+				FauxObjectMeta: fauxargocd.FauxObjectMeta{
+					Name:      input.crName,
+					Namespace: input.crNamespace,
+				},
+				Spec: fauxargocd.FauxApplicationSpec{
+					Source: fauxargocd.ApplicationSource{
+						RepoURL:        input.sourceRepoURL,
+						Path:           input.sourcePath,
+						TargetRevision: input.sourceTargetRevision,
+					},
+					Destination: fauxargocd.ApplicationDestination{
+						Name:      input.destinationName,
+						Namespace: input.destinationNamespace,
+					},
+					Project: "default",
+				},
+			}
+			if automated {
+				application.Spec.SyncPolicy = &fauxargocd.SyncPolicy{
+					Automated: &fauxargocd.SyncPolicyAutomated{
+						Prune:      true,
+						AllowEmpty: true,
+						SelfHeal:   true,
+					},
+				}
+			}
+
+			appBytes, err := yaml.Marshal(application)
+			if err != nil {
+				GinkgoT().Fatalf("failed to unmarshall Application: %q", err)
+			}
+
+			return string(appBytes)
+		}
+
 		It("Input spec is converted to an argocd Application", func() {
 			input := getfakeArgoCDSpecInput(false, false)
 			application, err := createSpecField(input)
 			Expect(err).To(BeNil())
-			Expect(application).To(Equal(validApplication))
+			Expect(application).To(Equal(getValidApplication(false)))
 		})
 
 		It("Sanitize illegal characters from input", func() {
 			input := getfakeArgoCDSpecInput(false, true)
 			application, err := createSpecField(input)
 			Expect(err).To(BeNil())
-			Expect(application).To(Equal(validApplication))
+			Expect(application).To(Equal(getValidApplication(false)))
 		})
 
 		It("Input spec with automated enabled should set automated sync policy", func() {
 			input := getfakeArgoCDSpecInput(true, false)
 			application, err := createSpecField(input)
 			Expect(err).To(BeNil())
-			Expect(application).To(Equal(validApplicationWithAutomatedSync))
+			Expect(application).To(Equal(getValidApplication(true)))
 		})
 	})
 })

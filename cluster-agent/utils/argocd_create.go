3935a2114080df675bebe70f43ebe819edf9fcef
Raghavi Shirur <raghaviss11@gmail.com>
2022-06-24 15:47:02 +0530

    Added changes to k8s API calls, wait.Poll to avoid race condition

--- cluster-agent/utils/argocd_create.go
+++ cluster-agent/utils/argocd_create.go
@@ -4,20 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"time"
+
+	"github.com/go-logr/logr"
 
 	argocdoperator "github.com/argoproj-labs/argocd-operator/api/v1alpha1"
 	appv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	openshiftv1 "github.com/openshift/api/route/v1"
 	v1 "github.com/openshift/api/route/v1"
+	sharedutil "github.com/redhat-appstudio/managed-gitops/backend-shared/util"
 	corev1 "k8s.io/api/core/v1"
 	rbac "k8s.io/api/rbac/v1"
 	apierr "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -26,12 +27,21 @@ const (
 	ArgoCDManagerServiceAccount     = "argocd-manager"
 	ArgoCDManagerClusterRole        = "argocd-manager-role"
 	ArgoCDManagerClusterRoleBinding = "argocd-manager-role-binding"
+	ArgoCDNamespace                 = "kube-system"
 	// K8sClientError is a prefix that can/should be used when outputting errors from K8s client
 	K8sClientError    = "Error from k8s client:"
 	ClusterSecretName = "my-cluster"
 )
 
-func CreateNamespaceScopedArgoCD(ctx context.Context, name string, namespace string, k8sClient client.Client) error {
+type ClusterSecretTLSClientConfigJSON struct {
+	Insecure bool `json:"insecure"`
+}
+type ClusterSecretConfigJSON struct {
+	BearerToken     string                           `json:"bearerToken"`
+	TLSClientConfig ClusterSecretTLSClientConfigJSON `json:"tlsClientConfig"`
+}
+
+func CreateNamespaceScopedArgoCD(ctx context.Context, name string, namespace string, k8sClient client.Client, log logr.Logger) error {
 	policy := "g, system:authenticated, role:admin"
 	scopes := "[groups]"
 
@@ -193,30 +203,30 @@ func CreateNamespaceScopedArgoCD(ctx context.Context, name string, namespace str
 	}
 
 	// Creating namespace
-	config := ctrl.GetConfigOrDie()
+	config, err := ctrl.GetConfig()
+	if err != nil {
+		return err
+	}
 	config.Timeout = time.Duration(10 * time.Second)
-	clientset := kubernetes.NewForConfigOrDie(config)
 
 	namespaceToCreate := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: namespace,
 		},
 	}
-	_, err := clientset.CoreV1().Namespaces().Create(context.Background(), namespaceToCreate, metav1.CreateOptions{})
+	err = k8sClient.Create(ctx, namespaceToCreate)
 	if err != nil {
-		fmt.Println("Namespace could not be created")
-		return err
+		return fmt.Errorf("Namespace could not be created: %v", err)
 	}
 
 	errk8s := k8sClient.Create(ctx, &argoCDOperand)
 	if errk8s != nil {
-		fmt.Println(K8sClientError, "Error on creating ", argoCDOperand.GetName(), errk8s)
-		return errk8s
+		return fmt.Errorf("Error on creating: %s, %v ", argoCDOperand.GetName(), errk8s)
 	}
 
 	// Wait for Argo CD to be installed by gitops operator. Use wait.Poll for ths
 
-	err = wait.Poll(1*time.Second, 60*time.Second, func() (bool, error) {
+	err = wait.Poll(1*time.Second, 3*time.Minute, func() (bool, error) {
 
 		appProject := &appv1.AppProject{
 			ObjectMeta: metav1.ObjectMeta{
@@ -227,8 +237,10 @@ func CreateNamespaceScopedArgoCD(ctx context.Context, name string, namespace str
 		err := k8sClient.Get(ctx, client.ObjectKeyFromObject(appProject), appProject)
 		if err != nil {
 			if apierr.IsNotFound(err) {
+				log.V(sharedutil.LogLevel_Debug).Info("Waiting for AppProject to exist in namespace " + namespace)
 				return false, nil
 			} else {
+				log.Error(err, "unable to retrieve AppProject")
 				return false, err
 			}
 		} else {
@@ -244,40 +256,32 @@ func CreateNamespaceScopedArgoCD(ctx context.Context, name string, namespace str
 	return nil
 }
 
-func SetupArgoCD(argoCDNamespace string, k8sClient client.Client, kubeClientSet *kubernetes.Clientset) error {
-
-	policy := metav1.DeletePropagationForeground
+func SetupArgoCD(ctx context.Context, argoCDNamespace string, k8sClient client.Client, log logr.Logger) error {
 
 	serviceAccount := &corev1.ServiceAccount{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "argocd-manager",
-			Namespace: "kube-system",
+			Name:      ArgoCDManagerServiceAccount,
+			Namespace: ArgoCDNamespace,
 		},
 	}
-	// Delete the resources, if it exists
 
-	_, errOnGet := kubeClientSet.CoreV1().ServiceAccounts(serviceAccount.Namespace).Get(context.Background(), serviceAccount.Name, metav1.GetOptions{})
-	if errOnGet == nil {
-		errOnDelete := kubeClientSet.CoreV1().ServiceAccounts(serviceAccount.Namespace).Delete(context.Background(), serviceAccount.Name, metav1.DeleteOptions{PropagationPolicy: &policy})
-		if errOnDelete != nil {
-			return fmt.Errorf("error on DELETE %v", errOnDelete)
+	err := k8sClient.Create(context.Background(), serviceAccount, &client.CreateOptions{})
+	if err != nil {
+		// If it already exists, then we update it
+		if apierr.IsAlreadyExists(err) {
+			if err := k8sClient.Update(context.Background(), serviceAccount); err != nil {
+				return fmt.Errorf("error on Update %v", err)
+			}
+		} else {
+			return fmt.Errorf("error on Create %v", err)
 		}
-
 	}
-
-	if err := k8sClient.Create(context.Background(), serviceAccount); err != nil {
-		if !apierr.IsAlreadyExists(err) {
-			return fmt.Errorf("failed to create service account %q in namespace %q: %v", serviceAccount.Name, serviceAccount.Namespace, err)
-		}
-		return fmt.Errorf("serviceAccount %q already exists in namespace %q", serviceAccount.Name, serviceAccount.Namespace)
-	}
-
-	log.Printf("serviceAccount %q created in namespace %q", serviceAccount.Name, serviceAccount.Namespace)
+	log.Info(fmt.Sprintf("serviceAccount %q created in namespace %q", serviceAccount.Name, serviceAccount.Namespace))
 
 	secret := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "argocd-manager-secret",
-			Namespace: "kube-system",
+			Namespace: ArgoCDNamespace,
 			Annotations: map[string]string{
 				"kubernetes.io/service-account.name": "argocd-manager",
 			},
@@ -286,18 +290,21 @@ func SetupArgoCD(argoCDNamespace string, k8sClient client.Client, kubeClientSet 
 	}
 
 	if err := k8sClient.Create(context.Background(), secret); err != nil {
-		if !apierr.IsAlreadyExists(err) {
-			return fmt.Errorf("failed to create secret %q in namespace %q: %v", secret.Name, secret.Namespace, err)
+		if apierr.IsAlreadyExists(err) {
+			if err := k8sClient.Update(context.Background(), secret); err != nil {
+				return fmt.Errorf("error on Update %v", err)
+			}
+		} else {
+			return fmt.Errorf("error on Create %v", err)
 		}
-		return fmt.Errorf("secret %q already exists in namespace %q", secret.Name, secret.Namespace)
 	}
 
-	log.Printf("secret %q created in namespace %q", secret.Name, secret.Namespace)
+	log.Info(fmt.Sprintf("secret %q created in namespace %q", secret.Name, secret.Namespace))
 
 	clusterRole := rbac.ClusterRole{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "argocd-manager-role",
-			Namespace: "kube-system",
+			Name:      ArgoCDManagerClusterRole,
+			Namespace: ArgoCDNamespace,
 		},
 		Rules: []rbac.PolicyRule{
 			{
@@ -309,97 +316,88 @@ func SetupArgoCD(argoCDNamespace string, k8sClient client.Client, kubeClientSet 
 		},
 	}
 
-	// Delete the resources, if they exists
-
-	_, errOnGet = kubeClientSet.RbacV1().ClusterRoles().Get(context.Background(), clusterRole.Name, metav1.GetOptions{})
-	if errOnGet == nil {
-		errOnDelete := kubeClientSet.RbacV1().ClusterRoles().Delete(context.Background(), clusterRole.Name, metav1.DeleteOptions{PropagationPolicy: &policy})
-		if errOnDelete != nil {
-			return fmt.Errorf("Error on DELETE %v", errOnDelete)
-		}
-
-	}
 	if err := k8sClient.Create(context.Background(), &clusterRole); err != nil {
-		if !apierr.IsAlreadyExists(err) {
-			return fmt.Errorf("failed to create clusterRole %q in namespace %q: %v", clusterRole.Name, clusterRole.Namespace, err)
+		if apierr.IsAlreadyExists(err) {
+			if err := k8sClient.Update(context.Background(), &clusterRole); err != nil {
+				return fmt.Errorf("error on Update %v", err)
+			}
+		} else {
+			return fmt.Errorf("error on Create %v", err)
 		}
-		return fmt.Errorf("clusterRole %q already exists in namespace %q", clusterRole.Name, clusterRole.Namespace)
 	}
-	log.Printf("clusterRole %q created in namespace %q", clusterRole.Name, clusterRole.Namespace)
+	log.Info(fmt.Sprintf("clusterRole %q created in namespace %q", clusterRole.Name, clusterRole.Namespace))
 
 	clusterRoleBinding := rbac.ClusterRoleBinding{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "argocd-manager-role-binding",
-			Namespace: "kube-system",
+			Name:      ArgoCDManagerClusterRoleBinding,
+			Namespace: ArgoCDNamespace,
 		},
 		RoleRef: rbac.RoleRef{
 			APIGroup: "rbac.authorization.k8s.io",
 			Kind:     "ClusterRole",
-			Name:     "argocd-manager-role",
+			Name:     ArgoCDManagerClusterRole,
 		},
 		Subjects: []rbac.Subject{
 			{
 				Kind:      "ServiceAccount",
-				Name:      "argocd-manager",
-				Namespace: "kube-system",
+				Name:      ArgoCDManagerServiceAccount,
+				Namespace: ArgoCDNamespace,
 			},
 		},
 	}
 
-	// debug : cluster role binding doesn't respond to the below code, need to delete manually using kubectl.
-	_, errOnGet = kubeClientSet.RbacV1().ClusterRoleBindings().Get(context.Background(), clusterRoleBinding.Name, metav1.GetOptions{})
-	if errOnGet == nil {
-		errOnDelete := kubeClientSet.RbacV1().ClusterRoleBindings().Delete(context.Background(), clusterRoleBinding.Name, metav1.DeleteOptions{PropagationPolicy: &policy})
-		if errOnDelete != nil {
-			return fmt.Errorf("Error on DELETE %v", errOnDelete)
-		}
-	}
 	if err := k8sClient.Create(context.Background(), &clusterRoleBinding); err != nil {
-		if !apierr.IsAlreadyExists(err) {
-			return fmt.Errorf("failed to create clusterRoleBinding %q in namespace %q: %v", clusterRoleBinding.Name, clusterRoleBinding.Namespace, err)
+		if apierr.IsAlreadyExists(err) {
+			if err := k8sClient.Update(context.Background(), &clusterRoleBinding); err != nil {
+				return fmt.Errorf("error on Update %v", err)
+			}
+		} else {
+			return fmt.Errorf("error on Create %v", err)
 		}
-		return fmt.Errorf("clusterRoleBinding %q already exists in namespace %q", clusterRoleBinding.Name, clusterRoleBinding.Namespace)
 	}
-	log.Printf("clusterRoleBinding %q created in namespace %q", clusterRoleBinding.Name, clusterRoleBinding.Namespace)
+	log.Info(fmt.Sprintf("clusterRoleBinding %q created in namespace %q", clusterRoleBinding.Name, clusterRoleBinding.Namespace))
 
-	// 	apiVersion: v1
-	// kind: Secret
-	// metadata:
-	//   name: my-cluster-secret
-	//   labels:
-	//     argocd.argoproj.io/secret-type: cluster
-	// type: Opaque
-	// stringData:
-	//   name: my-cluster
-	//   server: $API_URL
-	//   config: |
-	//     {
-	//       "bearerToken": "$SECRET_TOKEN",
-	//       "tlsClientConfig": {
-	//         "insecure": true
-	//       }
-	//     }
-
-	secretResource, secretErr := kubeClientSet.CoreV1().Secrets(secret.Namespace).Get(context.Background(), secret.Name, metav1.GetOptions{})
-	if secretErr != nil {
-		return secretErr
+	err = wait.Poll(1*time.Second, 3*time.Minute, func() (bool, error) {
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "argocd-manager-secret",
+				Namespace: ArgoCDNamespace,
+				Annotations: map[string]string{
+					"kubernetes.io/service-account.name": "argocd-manager",
+				},
+			},
+			Type: "kubernetes.io/service-account-token",
+		}
+		err := k8sClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
+		if err != nil {
+			if apierr.IsNotFound(err) {
+				log.V(sharedutil.LogLevel_Debug).Info("Waiting for Secret to exist in namespace " + secret.Namespace)
+				return false, nil
+			} else {
+				log.Error(err, "unable to retrieve Secret")
+				return false, err
+			}
+		} else {
+			if string(secret.Data["token"]) == "" {
+				log.Error(err, "Token byte stream is empty")
+			}
+			return true, nil
+		}
+	})
+	if err != nil {
+		fmt.Println("wait.Poll error : ", err)
+		return err
 	}
-	// token := secret.Data["token"]  not sure whether this will work
-	token := secretResource.Data["token"]
-	// no need to decode token, it is unmarshalled from base64
 
-	// decodedToken, err := base64.StdEncoding.DecodeString(string(token))
+	// err = k8sClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
 	// if err != nil {
-	// 	fmt.Printf("Error decoding Base64 encoded data %v", err)
+	// 	return err
 	// }
 
-	type ClusterSecretTLSClientConfigJSON struct {
-		Insecure bool `json:"insecure"`
-	}
-	type ClusterSecretConfigJSON struct {
-		BearerToken     string                           `json:"bearerToken"`
-		TLSClientConfig ClusterSecretTLSClientConfigJSON `json:"tlsClientConfig"`
-	}
+	// token := secret.Data["token"]  not sure whether this will work
+	token := secret.Data["token"]
+
+	// no need to decode token, it is unmarshalled from base64
 
 	clusterSecretConfigJSON := ClusterSecretConfigJSON{
 		BearerToken: string(token),
@@ -432,22 +430,19 @@ func SetupArgoCD(argoCDNamespace string, k8sClient client.Client, kubeClientSet 
 		},
 		Type: corev1.SecretType("Opaque"),
 	}
+
 	//delete cluster secret if it already exists to create a new one
-	_, errOnGet = kubeClientSet.CoreV1().Secrets(clusterSecret.Namespace).Get(context.Background(), clusterSecret.Name, metav1.GetOptions{})
-	if errOnGet == nil {
-		errOnDelete := kubeClientSet.CoreV1().Secrets(clusterSecret.Namespace).Delete(context.Background(), clusterSecret.Name, metav1.DeleteOptions{PropagationPolicy: &policy})
-		if errOnDelete != nil {
-			return fmt.Errorf("error on DELETE %v", errOnDelete)
-		}
 
-	}
 	if err := k8sClient.Create(context.Background(), clusterSecret); err != nil {
-		if !apierr.IsAlreadyExists(err) {
-			return fmt.Errorf("failed to create cluster secret %q : %v", clusterSecret.Name, err)
+		if apierr.IsAlreadyExists(err) {
+			if err := k8sClient.Update(context.Background(), clusterSecret); err != nil {
+				return fmt.Errorf("error on Update %v", err)
+			}
+		} else {
+			return fmt.Errorf("error on Create %v", err)
 		}
-		return fmt.Errorf("cluster secret %q already exists", clusterSecret.Name)
 	}
 
-	log.Printf("cluster secret %q created ", clusterSecret.Name)
+	log.Info(fmt.Sprintf("cluster secret %q created ", clusterSecret.Name))
 	return nil
 }

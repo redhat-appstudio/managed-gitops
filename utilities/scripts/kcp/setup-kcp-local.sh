af99c63d93f5304bc356b992a5bd4e0bda3f8731
Samyak Jain <samyak.jn11@gmail.com>
2022-07-04 16:20:30 +0530

    Add function to start kcp, add workload cluster, create workspace and install argo to kcp pt2
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- utilities/scripts/kcp/setup-kcp-local.sh
+++ utilities/scripts/kcp/setup-kcp-local.sh
@@ -62,36 +62,98 @@ kcp-init() {
   printf "\nInitiating KCP server ...\n"
   pushd "${TMP_DIR}"
   pwd
-  exec "$($KCP_DIR/bin/kcp start)" &> "${TMP_DIR}/kcp.log" &
+  "$($KCP_DIR/bin/kcp start)" &> "${TMP_DIR}/kcp.log" &
   KCP_PID=$!
-  
-  
   printf "KCP server started: %s\n" $KCP_PID
-
   touch "${TMP_DIR}/kcp-started"
+  printf "\n\n----------------------------------\n\n"
+  printf "\nKCP ready: %s\n" $?
 
-  # wait_command "ls ${KUBECONFIG}" 30
-  
-  # printf "Waiting for KCP to be ready ...\n"
-  
-  # wait_command "kubectl --kubeconfig=${KUBECONFIG} get --raw /readyz" 30
-  
-  printf "KCP ready: %s\n" $?
 }
 
+# wait for the passed function to complete the expected result with timeout instructions
+wait_for() {
+    timeout=$1
+    shift 1
+    until [ $timeout -le 0 ] || ("$@" &> /dev/null); do
+        echo waiting for "$@"
+        sleep 1
+        timeout=$(( timeout - 1 ))
+    done
+    if [ $timeout -le 0 ]; then
+        return 1
+    fi
+}
+
+# checks if the KCP is in ready state to proceed further with development
+check_if_kcp_is_ready() {
+    status=$(kubectl --kubeconfig=${TMP_DIR}/.kcp/admin.kubeconfig get --raw /readyz)
+    if [ "$status" == "ok" ]; then
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+# CRDs to sync to kcp and workload clusters
+CR_TOSYNC=(
+            # requirement of argocd installation
+            deployments.apps
+            pods
+            services
+            secrets
+            statefulsets
+            networkpolicies
+            configmaps
+            roles
+            rolebindings
+            clusterroles
+            clusterrolebindings
+            # requirement of gitops service
+            gitopsdeployments
+          )
+
+# Adds a workload cluster to KCP
+add-workload-cluster() {
+  exit_if_binary_not_installed "kubectl kcp"
+  # First, we need to add a workloadcluster to the kcp in order to sync resources
+  cat $(kubectl -v 99 config current-context 2>&1 | grep file: | awk -F ': ' '{print $2}' | xargs) > ${TMP_DIR}/local_kubeconfig
+  KUBECONFIG=${TMP_DIR}/.kcp/admin.kubeconfig sed -e 's/^/    /' ${TMP_DIR}/local_kubeconfig | cat ./manifests/workloadcluster.yaml - | kubectl apply -f -
+
+  # Then we add a syncer to kcp to sync the given resources to sync
+  cr_string="$(IFS=,; echo "${CR_TOSYNC[*]}")"
+  # kubectl kcp workload sync <workload-cluster-name> --syncer-image <kcp-syncer-image>
+  KUBECONFIG="$KUBECONFIG_KCP" kubectl kcp workload sync local \
+  --syncer-image ghcr.io/kcp-dev/kcp/syncer:release-0.5 \ # we are using the stable release-0.5 tag for this
+  --resources "$cr_string" > "${TMP_DIR}/syncer.yaml"
+  kubectl apply -f "${TMP_DIR}/syncer.yaml" >/dev/null 2>&1
+}
+
+# install ArgoCD manifests in the argocd  namespace
+install-argocd-in-kcp() {
+    KUBECONFIG=${TMP_DIR}/.kcp/admin.kubeconfig kubectl create ns argocd
+    KUBECONFIG=${TMP_DIR}/.kcp/admin.kubeconfig kubectl apply -f https://gist.githubusercontent.com/samyak-jn/192b754b8a079cca53478b487a7242fe/raw/6e48401f08357bc53fb68c86915f785e933e686e/installArgoForKCP.yaml -n argocd
+}
+
+
+# creates a development workspace (ex: dev-work)
 create-workspace() {
     printf "Creating a org dev workspace: dev-work"
     kcp_ws="dev-work"
     exit_if_binary_not_installed "kubectl kcp"
-    kubectl kcp workspaces use $kcp_ws
+    KUBECONFIG=${TMP_DIR}/.kcp/admin.kubeconfig kubectl kcp workspaces create $kcp_ws --enter
 }
 
 # Steps to setup KCP
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
 install-build-kcp-binaries
-
 TMP_DIR="$(mktemp -d -t kcp-gitops-service.XXXXXXXXX)"
 printf "Temporary directory created: %s\n" "${TMP_DIR}"
-
 kcp-init $TMP_DIR $KCP_DIR
+wait_for 30 check_if_kcp_is_ready $TMP_DIR
+# Once, KCP is up and running add a workloadcluster to it
+add-workload-cluster $TMP_DIR
+# Create a workspace for development purposes
+create-workspace $TMP_DIR
+# installs argocd in KCP workspace
+install-argocd-in-kcp $TMP_DIR

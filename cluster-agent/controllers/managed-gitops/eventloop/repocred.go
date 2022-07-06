f708e11d5ada04ad76dd65d0acfe102bd74ff571
Panagiotis Georgiadis <pgeorgia@redhat.com>
2022-07-06 20:39:13 +0200

    Fix the test

--- cluster-agent/controllers/managed-gitops/eventloop/repocred.go
+++ cluster-agent/controllers/managed-gitops/eventloop/repocred.go
@@ -10,17 +10,16 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"reflect"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
 	errOperationIDNotFound   = "resource ID was nil while processing operation: \"%s\""
-	errGenericDB             = "unable to retrieve database \"%s\" row from database"
-	errRowNotFound           = "the \"%s\" row no longer exists in the database"
-	errPrivateSecretNotFound = "Argo CD Private Repository secret doesn't exist: \"%s\""
-	errPrivateSecretCreate   = "Unable to create Argo CD Repository secret: \"%s\""
-	errGetPrivateSecret      = "unexpected error on retrieve Argo CD secret: \"%s\""
+	errGenericDB             = "unable to retrieve database row from database"
+	errRowNotFound           = "row no longer exists in the database"
+	errPrivateSecretNotFound = "Argo CD Private Repository secret doesn't exist:"
+	errPrivateSecretCreate   = "Unable to create Argo CD Repository secret:"
+	errGetPrivateSecret      = "unexpected error on retrieve Argo CD secret:"
 	errUpdatePrivateSecret   = "unable to update Argo CD Private Repository secret's \"%s\""
 )
 
@@ -31,19 +30,17 @@ func processOperation_RepositoryCredentials(ctx context.Context, dbOperation db.
 	argoCDNamespace corev1.Namespace, eventClient client.Client, l logr.Logger) (bool, error) {
 	const retry, noRetry = true, false
 
-	fmt.Println("")
-	fmt.Println("HELLOOOOOOO WORLD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-	fmt.Println("")
-
 	if dbOperation.Resource_id == "" {
 		return retry, fmt.Errorf(errOperationIDNotFound, crOperation.Name)
 	}
+
+	l = l.WithValues("operationRow", dbOperation.Operation_id)
 
 	// 2) Retrieve the RepositoryCredentials CR from the database.
 	dbRepositoryCredentials, err := dbQueries.GetRepositoryCredentialsByID(ctx, dbOperation.Resource_id)
 	if err != nil {
 		if db.IsResultNotFoundError(err) {
-			l.Error(err, errRowNotFound, dbOperation.Resource_id)
+			l.Error(err, errRowNotFound, "resource-id", dbOperation.Resource_id)
 			// TODO: Delete the corresponding repositoryCredentials CR, if it exists
 			// TODO: Add a finalizer to the CR, to make sure it is deleted only when the corresponding ArgoCD secret has been deleted as well
 			// // 1. Find the associated RepostitoryCredentials CR (How?)
@@ -68,12 +65,14 @@ func processOperation_RepositoryCredentials(ctx context.Context, dbOperation db.
 			return noRetry, err
 		} else {
 			// some other generic error
-			l.Error(err, errGenericDB, dbOperation.Resource_id)
+			l.Error(err, errGenericDB, "resource-id", dbOperation.Resource_id)
 			return retry, err
 		}
 	}
 
 	l = l.WithValues("repositoryCredentialsRow", dbRepositoryCredentials.RepositoryCredentialsID)
+	l.Info("Retrieved RepositoryCredentials CR from database")
+	l.Info(fmt.Sprintf("%v", dbRepositoryCredentials))
 
 	// 3) Retrieve ArgoCD secret from the cluster.
 	argoCDSecret := &corev1.Secret{
@@ -84,80 +83,108 @@ func processOperation_RepositoryCredentials(ctx context.Context, dbOperation db.
 	}
 
 	l = l.WithValues("ArgoCD Repository Secret", argoCDSecret.Name)
+	l.Info("Retrieving ArgoCD Repository Secret from cluster")
 
 	if err = eventClient.Get(ctx, client.ObjectKeyFromObject(argoCDSecret), argoCDSecret); err != nil {
 		if apierr.IsNotFound(err) {
-			l.Error(err, errPrivateSecretNotFound, argoCDSecret.Name)
-			l.Info("Creating Argo CD Private Repository secret", "secret", argoCDSecret.Name)
+			l.Info(errPrivateSecretNotFound, "Secret Name:", argoCDSecret.Name)
+			l.Info("Creating Argo CD Private Repository secret", "Secret Name:", argoCDSecret.Name)
 
 			repoCredToSecret(dbRepositoryCredentials, argoCDSecret)
 
 			errCreateArgoCDSecret := eventClient.Create(ctx, argoCDSecret, &client.CreateOptions{})
 			if errCreateArgoCDSecret != nil {
-				l.Error(errCreateArgoCDSecret, errPrivateSecretCreate, argoCDSecret.Name)
+				l.Error(errCreateArgoCDSecret, errPrivateSecretCreate, "Secret name", argoCDSecret.Name)
+				return retry, errCreateArgoCDSecret
 			}
 
-			return retry, errCreateArgoCDSecret
+			// The problem with the secret is now resolved, so we can proceed with the operation.
+			l.Info("Created Argo CD Private Repository secret successfully", "Secret name", argoCDSecret.Name)
+			l.Info(fmt.Sprintf("%v", argoCDSecret))
 
 		} else {
-			l.Error(err, errGetPrivateSecret, argoCDSecret.Name)
+			l.Error(err, errGetPrivateSecret, "Secret Name:", argoCDSecret.Name)
 			return retry, err
 		}
 	}
 
+	l.Info("Retrieved ArgoCD Repository Secret from cluster. Checking if it needs to be updated")
 	// 4. Check if the Argo CD secret has the correct data, and if not, update it with the data from the database.
 	// https://argo-cd.readthedocs.io/en/stable/operator-manual/declarative-setup/#repositories
-	if !reflect.DeepEqual(argoCDSecret.Name, dbRepositoryCredentials.SecretObj) {
+
+	l.Info(fmt.Sprintf("Labels: %s", argoCDSecret.Labels))
+	l.Info(fmt.Sprintf("Annotations: %s", argoCDSecret.Annotations))
+
+	decodedSecret := secretToRepoCred(argoCDSecret)
+	fmt.Println("Name:", decodedSecret.SecretObj)
+	fmt.Println("URL", decodedSecret.PrivateURL)
+	fmt.Println("Username", decodedSecret.AuthUsername)
+	fmt.Println("Password", decodedSecret.AuthPassword)
+	fmt.Println("SSH", decodedSecret.AuthSSHKey)
+
+	l.Info("Checking if the Name of the Argo CD Private Repository secret needs to be updated")
+	if decodedSecret.SecretObj != dbRepositoryCredentials.SecretObj {
 		l.Info("Updating Argo CD Private Repository secret", "secret name", argoCDSecret.Name)
 		argoCDSecret.Name = dbRepositoryCredentials.SecretObj
 		if err = eventClient.Update(ctx, argoCDSecret); err != nil {
 			l.Error(err, errUpdatePrivateSecret, "name")
 			return retry, err
 		}
+	} else {
+		l.Info("Re: No need the Name of the Argo CD Private Repository secret is the same with the respective RepositoryCredentials db entry")
 	}
 
-	if argoCDSecret.StringData["url"] != "" {
-		if !reflect.DeepEqual(argoCDSecret.StringData["url"], dbRepositoryCredentials.PrivateURL) {
-			l.Info("Updating Argo CD Private Repository secret", "URL", dbRepositoryCredentials.PrivateURL)
-			argoCDSecret.StringData["url"] = dbRepositoryCredentials.PrivateURL
-			if err = eventClient.Update(ctx, argoCDSecret); err != nil {
-				l.Error(err, errUpdatePrivateSecret, "URL")
-				return retry, err
-			}
+	//argoCDSecret.Data["url"] = []byte("https://www.google.com")
+	//_ = eventClient.Update(ctx, argoCDSecret)
+	//decodedSecret = secretToRepoCred(argoCDSecret)
+
+	l = l.WithValues("ArgoCD Repository Secret", argoCDSecret.Name)
+	l.Info("Checking if the URL of the Argo CD Private Repository secret needs to be updated")
+	if decodedSecret.PrivateURL != dbRepositoryCredentials.PrivateURL {
+		l.Info("Re: Yes, updating Argo CD Private Repository secret URL", "From (Current)", string(argoCDSecret.Data["url"]), "to (Database)", dbRepositoryCredentials.PrivateURL)
+		argoCDSecret.Data["url"] = []byte(dbRepositoryCredentials.PrivateURL)
+		if err = eventClient.Update(ctx, argoCDSecret); err != nil {
+			l.Error(err, errUpdatePrivateSecret, "url")
+			return retry, err
 		}
+	} else {
+		l.Info("Re: No need the URL of the Argo CD Private Repository secret is the same with the respective RepositoryCredentials db entry")
 	}
 
-	if argoCDSecret.StringData["password"] != "" {
-		if !reflect.DeepEqual(argoCDSecret.StringData["password"], dbRepositoryCredentials.AuthPassword) {
-			l.Info("Updating Argo CD Private Repository secret", "password", dbRepositoryCredentials.AuthPassword)
-			argoCDSecret.StringData["password"] = dbRepositoryCredentials.AuthPassword
-			if err = eventClient.Update(ctx, argoCDSecret); err != nil {
-				l.Error(err, errUpdatePrivateSecret, "password")
-				return retry, err
-			}
+	l.Info("Checking if the password of the Argo CD Private Repository secret needs to be updated")
+	if decodedSecret.AuthPassword != dbRepositoryCredentials.AuthPassword {
+		l.Info("Updating Argo CD Private Repository secret password", "secret name", argoCDSecret.Name)
+		argoCDSecret.Data["password"] = []byte(dbRepositoryCredentials.AuthPassword)
+		if err = eventClient.Update(ctx, argoCDSecret); err != nil {
+			l.Error(err, errUpdatePrivateSecret, "password")
+			return retry, err
 		}
+	} else {
+		l.Info("Re: No need the password of the Argo CD Private Repository secret is the same with the respective RepositoryCredentials db entry")
 	}
 
-	if argoCDSecret.StringData["username"] != "" {
-		if !reflect.DeepEqual(argoCDSecret.StringData["username"], dbRepositoryCredentials.AuthUsername) {
-			l.Info("Updating Argo CD Private Repository secret", "username", dbRepositoryCredentials.AuthPassword)
-			argoCDSecret.StringData["username"] = dbRepositoryCredentials.AuthPassword
-			if err = eventClient.Update(ctx, argoCDSecret); err != nil {
-				l.Error(err, errUpdatePrivateSecret, "username")
-				return retry, err
-			}
+	l.Info("Checking if the username of the Argo CD Private Repository secret needs to be updated")
+	if decodedSecret.AuthUsername != dbRepositoryCredentials.AuthUsername {
+		l.Info("Updating Argo CD Private Repository secret", "secret name", argoCDSecret.Name)
+		argoCDSecret.Data["username"] = []byte(dbRepositoryCredentials.AuthUsername)
+		if err = eventClient.Update(ctx, argoCDSecret); err != nil {
+			l.Error(err, errUpdatePrivateSecret, "username")
+			return retry, err
 		}
+	} else {
+		l.Info("Re: No need the username of the Argo CD Private Repository secret is the same with the respective RepositoryCredentials db entry")
 	}
 
-	if argoCDSecret.StringData["sshPrivateKey"] != "" {
-		if !reflect.DeepEqual(argoCDSecret.StringData["sshPrivateKey"], dbRepositoryCredentials.AuthSSHKey) {
-			l.Info("Updating Argo CD Private Repository secret", "sshPrivateKey", dbRepositoryCredentials.AuthPassword)
-			argoCDSecret.StringData["sshPrivateKey"] = dbRepositoryCredentials.AuthPassword
-			if err = eventClient.Update(ctx, argoCDSecret); err != nil {
-				l.Error(err, errUpdatePrivateSecret, "SSH PrivateKey")
-				return retry, err
-			}
+	l.Info("Checking if the SSH key of the Argo CD Private Repository secret needs to be updated")
+	if decodedSecret.AuthSSHKey != dbRepositoryCredentials.AuthSSHKey {
+		l.Info("Updating Argo CD Private Repository secret", "secret name", argoCDSecret.Name)
+		argoCDSecret.Data["ssh"] = []byte(dbRepositoryCredentials.AuthSSHKey)
+		if err = eventClient.Update(ctx, argoCDSecret); err != nil {
+			l.Error(err, errUpdatePrivateSecret, "ssh")
+			return retry, err
 		}
+	} else {
+		l.Info("Re: No need the SSH key of the Argo CD Private Repository secret is the same with the respective RepositoryCredentials db entry")
 	}
 
 	return noRetry, nil
@@ -165,8 +192,17 @@ func processOperation_RepositoryCredentials(ctx context.Context, dbOperation db.
 
 func repoCredToSecret(repoCred db.RepositoryCredentials, secret *corev1.Secret) {
 	if secret.Data == nil {
+		fmt.Printf("\n\nSecret is nil. Creating a map.\n\n")
 		secret.Data = make(map[string][]byte)
 	}
+
+	fmt.Println("This is the repoCred", repoCred)
+	fmt.Println("RepoCred name", repoCred.SecretObj)
+	fmt.Println("RepoCred URL", repoCred.PrivateURL)
+	fmt.Println("RepoCred username", repoCred.AuthUsername)
+	fmt.Println("RepoCred password", repoCred.AuthPassword)
+	fmt.Println("RepoCred SSH PrivateKey", repoCred.AuthSSHKey)
+	fmt.Println("Common Label:", common.LabelValueSecretTypeRepository)
 
 	updateSecretString(secret, "name", repoCred.SecretObj)
 	updateSecretString(secret, "url", repoCred.PrivateURL)
@@ -193,7 +229,8 @@ func repoCredToSecret(repoCred db.RepositoryCredentials, secret *corev1.Secret) 
 }
 
 func updateSecretString(secret *corev1.Secret, key, value string) {
-	if _, present := secret.Data[key]; present || len(value) > 0 {
+	if _, present := secret.Data[key]; present || value != "" {
+		fmt.Printf("\n\nUpdating Secret's %s\n\n", key)
 		secret.Data[key] = []byte(value)
 	}
 }
@@ -208,4 +245,19 @@ func addSecretMetadata(secret *corev1.Secret, secretType string) {
 		secret.Labels = map[string]string{}
 	}
 	secret.Labels[common.LabelKeySecretType] = secretType
+}
+
+// secretToRepoCred converts a Secret to a RepositoryCredentials
+// This is the reverse of repoCredToSecret and it's needed because by default the values of the secret are in bytes
+// e.g. Secret.name: [116 101 115 116 45 102 97 107 101 45 115 101 99 114 101 116 45 111 98 106]
+//		Secret password: [116 101 115 116 45 102 97 107 101 45 97 117 116 104 45 112 97 115 115 119 111 114 100]
+// that is why we need this function. To typecast the bytes to string.
+func secretToRepoCred(secret *corev1.Secret) (repoCred *db.RepositoryCredentials) {
+	return &db.RepositoryCredentials{
+		PrivateURL:   string(secret.Data["url"]),
+		AuthUsername: string(secret.Data["username"]),
+		AuthPassword: string(secret.Data["password"]),
+		AuthSSHKey:   string(secret.Data["sshPrivateKey"]),
+		SecretObj:    string(secret.Data["name"]),
+	}
 }
--- cluster-agent/controllers/managed-gitops/eventloop/repocred_test.go
+++ cluster-agent/controllers/managed-gitops/eventloop/repocred_test.go
@@ -8,9 +8,12 @@ import (
 	. "github.com/onsi/gomega"
 	operation "github.com/redhat-appstudio/managed-gitops/backend-shared/apis/managed-gitops/v1alpha1"
 	"github.com/redhat-appstudio/managed-gitops/backend-shared/config/db"
+	dbutil "github.com/redhat-appstudio/managed-gitops/backend-shared/config/db/util"
 	managedgitopsv1alpha1 "github.com/redhat-appstudio/managed-gitops/backend/apis/managed-gitops/v1alpha1"
 	"github.com/redhat-appstudio/managed-gitops/backend/eventloop/eventlooptypes"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -45,12 +48,27 @@ var _ = Describe("RepositoryCredentials Operation Tests", func() {
 			ctx = context.Background()
 			logger = log.FromContext(ctx) // do this, or invalid memory address or nil pointer dereference will occur
 
-			_, _, _, gitopsEngineInstance, _, err = db.CreateSampleData(dbq)
+			By("Get a clusterEngineInstance")
+			scheme, argocdNamespace, kubesystemNamespace, workspace, err := eventlooptypes.GenericTestSetup()
+			Expect(err).To(BeNil())
+
+			_, _, _, _, _, err = db.CreateSampleData(dbq)
+			Expect(err).To(BeNil())
+
+			gitopsEngineCluster, _, err := dbutil.GetOrCreateGitopsEngineClusterByKubeSystemNamespaceUID(ctx, string(kubesystemNamespace.UID), dbq, logger)
+			Expect(gitopsEngineCluster).ToNot(BeNil())
+			Expect(err).To(BeNil())
+
+			gitopsEngineInstance = &db.GitopsEngineInstance{
+				Gitopsengineinstance_id: "test-fake-engine-instance",
+				Namespace_name:          argocdNamespace.Name,
+				Namespace_uid:           string(argocdNamespace.UID),
+				EngineCluster_id:        gitopsEngineCluster.Gitopsenginecluster_id,
+			}
+			err = dbq.CreateGitopsEngineInstance(ctx, gitopsEngineInstance)
 			Expect(err).To(BeNil())
 
 			By("Satisfying the foreign key constraint 'fk_clusteruser_id'")
-			// aka: ClusterUser.Clusteruser_id) required for 'repo_cred_user_id'
-
 			clusterUser = &db.ClusterUser{
 				Clusteruser_id: "test-repocred-user-id",
 				User_name:      "test-repocred-user",
@@ -60,12 +78,6 @@ var _ = Describe("RepositoryCredentials Operation Tests", func() {
 
 			// To work with Kubernetes you need a client and a task
 			By("Initialize fake kube client")
-			//var workspace *v1.Namespace
-			//var argocdNamespace *v1.Namespace
-			//var kubesystemNamespace *v1.Namespace
-
-			scheme, argocdNamespace, kubesystemNamespace, workspace, err := eventlooptypes.GenericTestSetup()
-			Expect(err).To(BeNil())
 
 			err = appv1.AddToScheme(scheme)
 			Expect(err).To(BeNil())
@@ -95,12 +107,12 @@ var _ = Describe("RepositoryCredentials Operation Tests", func() {
 			dbq.CloseDatabase() // Close the database connection.
 		})
 
-		FIt("Ensure that calling perform task on an operation CR for RepositoryCredential that doesn't exist, it doesn't return an error, and retry is false", func() {
+		It("Ensure that calling perform task on an operation CR for RepositoryCredential that doesn't exist, it doesn't return an error, and retry is false", func() {
 			By("creating Operation row in database")
 			operationDB := &db.Operation{
 				Operation_id:            "test-operation",
 				Instance_id:             gitopsEngineInstance.Gitopsengineinstance_id,
-				Resource_id:             "test-fake-resource-id",
+				Resource_id:             "test-my-repo-creds", // this needs to be unique for this testcase!
 				Resource_type:           db.OperationResourceType_RepositoryCredentials,
 				State:                   db.OperationState_Waiting,
 				Operation_owner_user_id: clusterUser.Clusteruser_id,
@@ -119,12 +131,96 @@ var _ = Describe("RepositoryCredentials Operation Tests", func() {
 					OperationID: operationDB.Operation_id,
 				},
 			}
+
 			err = task.event.client.Create(ctx, operationCR)
 			Expect(err).To(BeNil())
 
+			// Create a fake RepositoryCredential (make sure you don't violate the constraints or the test will fail)
+			By("Creating a fake RepositoryCredential")
+			repositoryCredential := db.RepositoryCredentials{
+				RepositoryCredentialsID: operationDB.Resource_id,
+				UserID:                  clusterUser.Clusteruser_id, // comply with the constraint 'fk_clusteruser_id'
+				PrivateURL:              "test-fake-private-url",
+				AuthUsername:            "test-fake-auth-username",
+				AuthPassword:            "test-fake-auth-password",
+				AuthSSHKey:              "",
+				SecretObj:               "test-fake-secret-obj",
+				EngineClusterID:         gitopsEngineInstance.Gitopsengineinstance_id, // comply with the constraint 'fk_gitopsengineinstance_id'
+			}
+
+			// Inject the fake RepositoryCredential into the database
+			By("Injecting the fake RepositoryCredential into the database")
+			err = dbq.CreateRepositoryCredentials(ctx, &repositoryCredential)
+			Expect(err).To(BeNil())
+
+			By("Getting the RepositoryCredentials object from the database")
+			fetch, err := dbq.GetRepositoryCredentialsByID(ctx, operationDB.Resource_id)
+			Expect(err).To(BeNil())
+			Expect(fetch).Should(Equal(repositoryCredential))
+
+			// Do it
 			retry, err := task.PerformTask(ctx)
 			Expect(err).To(BeNil())
 			Expect(retry).To(BeFalse())
+
+			// Get the secret and check it
+			By("Get the secret")
+			secret := &corev1.Secret{}
+			err = task.event.client.Get(ctx, types.NamespacedName{Name: repositoryCredential.SecretObj, Namespace: namespace}, secret)
+			Expect(err).To(BeNil())
+			Expect(secret.Data).Should(HaveKey("url"))
+			Expect(secret.Data).Should(HaveKey("username"))
+			Expect(secret.Data).Should(HaveKey("password"))
+			Expect(string(secret.Data["url"])).Should(Equal(repositoryCredential.PrivateURL))
+			Expect(string(secret.Data["username"])).Should(Equal(repositoryCredential.AuthUsername))
+			Expect(string(secret.Data["password"])).Should(Equal(repositoryCredential.AuthPassword))
+
+			//// New Test Case: Change the secret
+			//// I have to somehow change the status back to "Progressing"
+			//// otherwise: --> Skipping Operation with state of completed/failed	{"operationId": "test-operation"}
+			//By("Changing the status of the operation to Waiting")
+			//operationDB.State = db.OperationState_Waiting
+			//err = dbq.UpdateOperation(ctx, operationDB)
+			//Expect(err).To(BeNil())
+			//
+			//By("creating Operation CR")
+			//operationCR = &operation.Operation{
+			//	ObjectMeta: metav1.ObjectMeta{
+			//		Name:      name,
+			//		Namespace: namespace,
+			//	},
+			//	Spec: operation.OperationSpec{
+			//		OperationID: operationDB.Operation_id,
+			//	},
+			//}
+			//
+			//err = task.event.client.Update(ctx, operationCR)
+			//Expect(err).To(BeNil())
+			//
+			//By("Changing the secret")
+			//secret.Data["url"] = []byte("test-fake-private-url-2")
+			//secret.Data["username"] = []byte("test-fake-auth-username-2")
+			//secret.Data["password"] = []byte("test-fake-auth-password-2")
+			//err = task.event.client.Update(ctx, secret)
+			//
+			//// Run again the task, it should fix the secret
+			//By("Running again the task, it should fix the secret")
+			//retry, err = task.PerformTask(ctx)
+			//Expect(err).To(BeNil())
+			//Expect(retry).To(BeFalse())
+			//
+			//// Check the secret if it's OK (in sync with the database)
+			//By("Get the secret")
+			//err = task.event.client.Get(ctx, types.NamespacedName{Name: repositoryCredential.SecretObj, Namespace: namespace}, secret)
+			//Expect(err).To(BeNil())
+			//By("Check the secret if it's OK (in sync with the database)")
+			//Expect(secret.Data).Should(HaveKey("url"))
+			//Expect(secret.Data).Should(HaveKey("username"))
+			//Expect(secret.Data).Should(HaveKey("password"))
+			//Expect(string(secret.Data["url"])).Should(Equal(repositoryCredential.PrivateURL))
+			//Expect(string(secret.Data["username"])).Should(Equal(repositoryCredential.AuthUsername))
+			//Expect(string(secret.Data["password"])).Should(Equal(repositoryCredential.AuthPassword))
+
 		})
 	})
 })

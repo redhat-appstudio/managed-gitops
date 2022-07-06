adcdd6d40bc111ac00bcf225b6e1c656612e864a
Raghavi Shirur <raghaviss11@gmail.com>
2022-07-06 14:58:33 +0530

    target function modification

--- tests-e2e/core/simple_test.go
+++ tests-e2e/core/simple_test.go
@@ -86,22 +86,7 @@ func buildGitOpsDeploymentResource(name, repoURL, path, deploymentSpecType strin
 }
 
 func buildTargetRevisionGitOpsDeploymentResource(name, repoURL, path, target, deploymentSpecType string) managedgitopsv1alpha1.GitOpsDeployment {
-
-	gitOpsDeploymentResource := managedgitopsv1alpha1.GitOpsDeployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: fixture.GitOpsServiceE2ENamespace,
-		},
-		Spec: managedgitopsv1alpha1.GitOpsDeploymentSpec{
-			Source: managedgitopsv1alpha1.ApplicationSource{
-				RepoURL:        repoURL,
-				Path:           path,
-				TargetRevision: target,
-			},
-			Destination: managedgitopsv1alpha1.ApplicationDestination{},
-			Type:        deploymentSpecType,
-		},
-	}
-
+	gitOpsDeploymentResource := buildGitOpsDeploymentResource(name, repoURL, path, deploymentSpecType)
+	gitOpsDeploymentResource.Spec.Source.TargetRevision = target
 	return gitOpsDeploymentResource
 }

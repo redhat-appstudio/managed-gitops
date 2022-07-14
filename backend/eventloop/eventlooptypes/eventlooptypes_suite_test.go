a29d70a778b86bfafdcef2294e7acb5a172f8f17
jparsai <jparsai@redhat.com>
2022-07-14 13:25:55 +0530

    Avoid creating duplicate Operations.

---
+++ backend/eventloop/eventlooptypes/eventlooptypes_suite_test.go
@@ -0,0 +1,13 @@
+package eventlooptypes_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestEventlooptypes(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Eventlooptypes Suite")
+}

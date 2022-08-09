168e7c9e1e28bfd1865fc67307cf5aaffb17e84a
Chetan Banavikalmutt <chetanrns1997@gmail.com>
2022-08-09 16:33:04 +0530

    Install argocd in a KCP workspace and run e2e tests

--- kcp/setup-kcp-cps.sh
+++ kcp/setup-kcp-cps.sh
@@ -2,45 +2,151 @@
 
 set -ex
 
-CPS_KUBECONFIG="${CPS_KUBECONFIG:-cps-kubeconfig}"
+CPS_KUBECONFIG="${CPS_KUBECONFIG:-$(realpath kcp/cps-kubeconfig)}"
 WORKLOAD_KUBECONFIG="${WORKLOAD_KUBECONFIG:-$HOME/.kube/config}"
 WORKSPACE="gitops-service"
-SYNCER_IMAGE="${SYNCER_IMAGE:-ghcr.io/kcp-dev/kcp/syncer:v0.5.0-alpha.1}"
+SYNCER_IMAGE="${SYNCER_IMAGE:-ghcr.io/kcp-dev/kcp/syncer:v0.6.4}"
 SYNCER_MANIFESTS=$(mktemp -d)/cps-syncer.yaml
 
-# Check if kubectl, kubelogin is installed
+ARGOCD_MANIFEST="https://gist.githubusercontent.com/chetan-rns/91d0b56af152f3ebb7c10df8e82b459d/raw/99429ff5ac68cb78a4fc70f1ac2d673ad7ba192a/install-argocd.yaml"
+ARGOCD_NAMESPACE="gitops-service-argocd"
+
+# Checks if a binary is present on the local system
+exit_if_binary_not_installed() {
+    for binary in "$@"; do
+        command -v "$binary" >/dev/null 2>&1 || {
+            echo >&2 "Script requires '$binary' command-line utility to be installed on your local machine. Aborting..."
+            exit 1
+        }
+    done
+}
+
+# Check if kubectl, kubelogin and kcp plugins are installed
+exit_if_binary_not_installed "kubectl" "kubectl-kcp" "kubectl-oidc_login"
+
 KUBECONFIG="$CPS_KUBECONFIG" kubectl config use-context kcp-stable
 
 # Opens a web browser to authenticate to your RH SSO acount
 KUBECONFIG="$CPS_KUBECONFIG" kubectl api-resources
 
+# Create a new workspace if it doesn't exist
+KUBECONFIG="$CPS_KUBECONFIG" kubectl get workspaces | grep "${WORKSPACE}"
+if [ $? -eq 1 ]; then 
+    KUBECONFIG="$CPS_KUBECONFIG" kubectl kcp ws create "$WORKSPACE"
+fi
 
-# Enter a new workspace and enter that workspace
-KUBECONFIG="$CPS_KUBECONFIG" kubectl kcp workspace create "$WORKSPACE" --enter --ignore-existing
+KUBECONFIG="$CPS_KUBECONFIG" kubectl kcp ws use "$WORKSPACE"
 
+KUBECONFIG="$CPS_KUBECONFIG" kubectl create ns kube-system || true
 
 # Extract the Workload Cluster name from the KUBECONFIG
 WORKLOAD_CLUSTER=$(KUBECONFIG="${WORKLOAD_KUBECONFIG}" kubectl config view --minify -o jsonpath='{.clusters[].name}' | awk -F[:] '{print $1}')
 
-KUBECONFIG="$CPS_KUBECONFIG" kubectl kcp workload sync "$WORKLOAD_CLUSTER" --syncer-image "$SYNCER_IMAGE" > "$SYNCER_MANIFESTS"
+WORKLOAD_CLUSTER=${WORKLOAD_CLUSTER:22}
+
+echo "Generating syncer manifests for OCP SyncTarget $WORKLOAD_CLUSTER"
+KUBECONFIG="$CPS_KUBECONFIG" kubectl kcp workload sync "$WORKLOAD_CLUSTER"  --resources "services,statefulsets.apps,deployments.apps,routes.route.openshift.io" --syncer-image "$SYNCER_IMAGE" --output-file "$SYNCER_MANIFESTS" --namespace kcp-syncer
 
 
-# Deploy the syncer to the Workload cluster
+# Deploy the syncer to the SyncTarget
 KUBECONFIG="${WORKLOAD_KUBECONFIG}" kubectl apply -f "$SYNCER_MANIFESTS"
 
 
-# Check if the cluster is already registered
+echo "Waiting for the SyncTarget resource to reach ready state"
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl wait --for=condition=Ready=true synctarget/"${WORKLOAD_CLUSTER}"
 
-# Wait until the Workload cluster is in ready state
-KUBECONFIG="${CPS_KUBECONFIG}" kubectl wait --for=condition=Ready=true workloadclusters/"${WORKLOAD_CLUSTER}"
+create_kubeconfig_secret() {
+    sa_name=$1
+    sa_secret_name=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get sa $sa_name -n $ARGOCD_NAMESPACE -o=jsonpath='{.secrets[0].name}')
 
-# KUBECONFIG="${CPS_KUBECONFIG}" kubectl get workloadclusters "$WORKLOAD_CLUSTER" -owide
+    ca=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get secret/$sa_secret_name -n $ARGOCD_NAMESPACE -o jsonpath='{.data.ca\.crt}')
+    token=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get secret/$sa_secret_name -n $ARGOCD_NAMESPACE -o jsonpath='{.data.token}' | base64 --decode)
+    namespace=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get secret/$sa_secret_name -n $ARGOCD_NAMESPACE -o jsonpath='{.data.namespace}' | base64 --decode)
+
+    server=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl config view -o jsonpath='{.clusters[?(@.name == "workspace.kcp.dev/current")].cluster.server}')
+
+    secret_name=$2
+    kubeconfig_secret="
+apiVersion: v1
+kind: Secret
+metadata:
+  creationTimestamp: null
+  name: ${secret_name}
+  namespace: ${ARGOCD_NAMESPACE}
+stringData:
+  kubeconfig: |
+    apiVersion: v1
+    kind: Config
+    clusters:
+    - name: default-cluster
+      cluster:
+        certificate-authority-data: ${ca}
+        server: ${server}
+    contexts:
+    - name: default-context
+      context:
+        cluster: default-cluster
+        namespace: ${ARGOCD_NAMESPACE}
+        user: default-user
+    current-context: default-context
+    users:
+    - name: default-user
+      user:
+        token: ${token}
+"
+
+    echo "${kubeconfig_secret}" | KUBECONFIG="${CPS_KUBECONFIG}" kubectl apply -f -
+}
+
+echo "Installing Argo CD resources in workspace $WORKSPACE and namespace $ARGOCD_NAMESPACE"
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl create ns $ARGOCD_NAMESPACE || true
+
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl apply -f $ARGOCD_MANIFEST -n $ARGOCD_NAMESPACE
+
+echo "Creating KUBECONFIG secrets for argocd server and argocd application controller service accounts"
+create_kubeconfig_secret "argocd-server" "kcp-kubeconfig-controller"
+create_kubeconfig_secret "argocd-application-controller" "kcp-kubeconfig-server"
+
+echo "Verifying if argocd components are up and running after mounting kubeconfig secrets"
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl wait --for=condition=available deployments/argocd-server -n $ARGOCD_NAMESPACE
+count=0
+while [ $count -lt 30 ]
+do
+    count=`expr $count + 1`
+    replicas=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get statefulsets argocd-application-controller -n $ARGOCD_NAMESPACE -o jsonpath='{.spec.replicas}')
+    ready_replicas=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get statefulsets argocd-application-controller -n $ARGOCD_NAMESPACE -o jsonpath='{.status.readyReplicas}')
+    if [ "$replicas" -eq "$ready_replicas" ]; then 
+        break
+    fi
+    if [ $count -eq 30 ]; then 
+        echo "Statefulset argocd-application-controller does not have the required replicas"
+        exit 1
+    fi
+done
 
 cleanup_workspace() {
+    echo "Deleting argocd resources from $ARGOCD_NAMESPACE"
+    KUBECONFIG="${CPS_KUBECONFIG}" kubectl delete -f $ARGOCD_MANIFEST -n gitops-service-argocd    
+
+    echo "Deleting syncer resources from the SyncTarget cluster"
     KUBECONFIG="${WORKLOAD_KUBECONFIG}" kubectl delete -f "$SYNCER_MANIFESTS"
-    # enter root workspace
-    KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace ..
+    
+    echo "Deleting KCP workspace $WORKSPACE"
+    KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws ..
     KUBECONFIG="${CPS_KUBECONFIG}" kubectl delete workspace "$WORKSPACE"
-}
+    
+    kill $E2E_SERVER_PID
+    ./stop-dev-env.sh
+    ./delete-dev-env.sh
+} 
+
+echo "Argo CD is successfully installed in namespace $ARGOCD_NAMESPACE"
+
+echo "Running gitops service controllers"
+KUBECONFIG="${CPS_KUBECONFIG}" make devenv-docker
+KUBECONFIG="${CPS_KUBECONFIG}" make start-e2e &
+E2E_SERVER_PID=$!
+
+KUBECONFIG="${CPS_KUBECONFIG}" make test-e2e
 
 cleanup_workspace
--- tests-e2e/fixture/fixture.go
+++ tests-e2e/fixture/fixture.go
@@ -243,6 +243,10 @@ func ensureDestinationNamespaceExists(namespaceParam string, argoCDNamespacePara
 		return err
 	}
 
+	if err = addMissingPermissions(kubeClientSet, namespaceParam, argoCDNamespaceParam); err != nil {
+		return nil
+	}
+
 	// Wait for Argo CD to process the namespace, before we exit:
 	// - This helps us avoid a race condition where the namespace is created, but Argo CD has not yet
 	//   set up proper roles for it.
@@ -299,6 +303,33 @@ func ensureDestinationNamespaceExists(namespaceParam string, argoCDNamespacePara
 		return false, nil
 	}); err != nil {
 		return fmt.Errorf("argo CD never setup rolebindings for namespace '%s': %v", namespaceParam, err)
+	}
+
+	// allow argocd to manage the target namespace
+	err = wait.Poll(time.Second*1, time.Minute*2, func() (done bool, err error) {
+		secretList, err := kubeClientSet.CoreV1().Secrets(argoCDNamespaceParam).List(context.Background(), metav1.ListOptions{
+			LabelSelector: "argocd.argoproj.io/secret-type=cluster",
+		})
+		if err != nil {
+			return false, err
+		}
+
+		if len(secretList.Items) > 0 {
+			clusterSecret := secretList.Items[0]
+
+			ns := []string{argoCDNamespaceParam, namespaceParam}
+			clusterSecret.Data["namespaces"] = []byte(strings.Join(ns, ","))
+
+			_, err = kubeClientSet.CoreV1().Secrets(argoCDNamespaceParam).Update(context.Background(), &clusterSecret, metav1.UpdateOptions{})
+			if err != nil {
+				return false, err
+			}
+		}
+
+		return true, nil
+	})
+	if err != nil {
+
 	}
 
 	return nil
@@ -361,6 +392,105 @@ func DeleteNamespace(namespaceParam string) error {
 			return true, nil
 		}); err != nil {
 			GinkgoWriter.Printf("unable to remove finalizer from Argo CD Applications in namespace '%s': %v\n", namespaceParam, err)
+			return false, nil
+		}
+
+		// Remove KCP finalizers from secrets in this namespace
+		if err := wait.Poll(time.Second*1, time.Minute*2, func() (done bool, err error) {
+
+			secretList := corev1.SecretList{}
+			if err = k8sClient.List(context.Background(), &secretList, &client.ListOptions{Namespace: namespaceParam}); err != nil {
+				GinkgoWriter.Println("unable to list secrets in '"+namespaceParam+"'", err)
+				return false, nil
+			}
+
+			for idx := range secretList.Items {
+
+				secret := secretList.Items[idx]
+				if len(secret.Finalizers) > 0 {
+					err = k8sClient.Get(context.Background(), client.ObjectKeyFromObject(&secret), &secret)
+					if err != nil {
+						GinkgoWriter.Println("unable to get Secret '"+secret.Name+"'", err)
+						return false, nil
+					}
+					secret.Finalizers = []string{}
+					err = k8sClient.Update(context.Background(), &secret)
+					if err != nil {
+						GinkgoWriter.Println("unable to update Secret '"+secret.Name+"'", err)
+						return false, nil
+					}
+				}
+			}
+
+			return true, nil
+		}); err != nil {
+			GinkgoWriter.Printf("unable to remove finalizer from  in namespace '%s': %v\n", namespaceParam, err)
+			return false, nil
+		}
+
+		// Remove KCP finalizers from service accounts in this namespace
+		if err := wait.Poll(time.Second*1, time.Minute*2, func() (done bool, err error) {
+
+			saList := corev1.ServiceAccountList{}
+			if err = k8sClient.List(context.Background(), &saList, &client.ListOptions{Namespace: namespaceParam}); err != nil {
+				GinkgoWriter.Println("unable to list service accounts in '"+namespaceParam+"'", err)
+				return false, nil
+			}
+
+			for idx := range saList.Items {
+
+				sa := saList.Items[idx]
+				if len(sa.Finalizers) > 0 {
+					err = k8sClient.Get(context.Background(), client.ObjectKeyFromObject(&sa), &sa)
+					if err != nil {
+						GinkgoWriter.Println("unable to get service account '"+sa.Name+"'", err)
+						return false, nil
+					}
+					sa.Finalizers = []string{}
+					err = k8sClient.Update(context.Background(), &sa)
+					if err != nil {
+						GinkgoWriter.Println("unable to update service account '"+sa.Name+"'", err)
+						return false, nil
+					}
+				}
+			}
+
+			return true, nil
+		}); err != nil {
+			GinkgoWriter.Printf("unable to remove finalizer from service account in namespace '%s': %v\n", namespaceParam, err)
+			return false, nil
+		}
+
+		// Remove KCP finalizers from configmaps in this namespace
+		if err := wait.Poll(time.Second*1, time.Minute*2, func() (done bool, err error) {
+
+			cmList := corev1.ConfigMapList{}
+			if err = k8sClient.List(context.Background(), &cmList, &client.ListOptions{Namespace: namespaceParam}); err != nil {
+				GinkgoWriter.Println("unable to list configmaps in '"+namespaceParam+"'", err)
+				return false, nil
+			}
+
+			for idx := range cmList.Items {
+
+				cm := cmList.Items[idx]
+				if len(cm.Finalizers) > 0 {
+					err = k8sClient.Get(context.Background(), client.ObjectKeyFromObject(&cm), &cm)
+					if err != nil {
+						GinkgoWriter.Println("unable to get configmap '"+cm.Name+"'", err)
+						return false, nil
+					}
+					cm.Finalizers = []string{}
+					err = k8sClient.Update(context.Background(), &cm)
+					if err != nil {
+						GinkgoWriter.Println("unable to update configmap '"+cm.Name+"'", err)
+						return false, nil
+					}
+				}
+			}
+
+			return true, nil
+		}); err != nil {
+			GinkgoWriter.Printf("unable to remove finalizer from configmap in namespace '%s': %v\n", namespaceParam, err)
 			return false, nil
 		}
 
@@ -510,6 +640,72 @@ func ReportRemainingArgoCDApplications(k8sClient client.Client) error {
 		GinkgoWriter.Printf("- %d) %s\n", idx+1, (string)(jsonStr))
 	}
 	GinkgoWriter.Println()
+
+	return nil
+}
+
+func addMissingPermissions(kubeClientSet *kubernetes.Clientset, namespace, argocdNamespace string) error {
+
+	addNamespacePrefix := func(name string) string {
+		return fmt.Sprintf("%s-%s", argocdNamespace, name)
+	}
+
+	getAdminRole := func(name string) *rbacv1.Role {
+		return &rbacv1.Role{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      addNamespacePrefix(name),
+				Namespace: namespace,
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					Verbs:     []string{"*"},
+					Resources: []string{"*"},
+					APIGroups: []string{"*"},
+				},
+			},
+		}
+	}
+
+	getRolebinding := func(name string) *rbacv1.RoleBinding {
+		return &rbacv1.RoleBinding{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      addNamespacePrefix(name),
+				Namespace: namespace,
+			},
+			Subjects: []rbacv1.Subject{
+				{
+					Name:      name,
+					Namespace: argocdNamespace,
+					Kind:      "ServiceAccount",
+				},
+			},
+			RoleRef: rbacv1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Name:     addNamespacePrefix(name),
+				Kind:     "Role",
+			},
+		}
+	}
+
+	_, err := kubeClientSet.RbacV1().Roles(namespace).Create(context.Background(), getAdminRole("argocd-server"), metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	_, err = kubeClientSet.RbacV1().Roles(namespace).Create(context.Background(), getAdminRole("argocd-application-controller"), metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	_, err = kubeClientSet.RbacV1().RoleBindings(namespace).Create(context.Background(), getRolebinding("argocd-server"), metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	_, err = kubeClientSet.RbacV1().RoleBindings(namespace).Create(context.Background(), getRolebinding("argocd-application-controller"), metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
 
 	return nil
 }

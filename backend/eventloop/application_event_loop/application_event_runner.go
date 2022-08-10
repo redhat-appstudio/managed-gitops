38fb02895e591249480ba1c6aa7c4da3b92fa56e
Raghavi Shirur <raghaviss11@gmail.com>
2022-08-10 17:21:02 +0530

    remove previous version of code

--- backend/eventloop/application_event_loop/application_event_runner.go
+++ backend/eventloop/application_event_loop/application_event_runner.go
@@ -420,33 +420,3 @@ type applicationEventLoopRunner_Action struct {
 
 	k8sClientFactory shared_resource_loop.SRLK8sClientFactory
 }
-
-// cleanupOperation cleans up the database entry and (optionally) the CR, once an operation has concluded.
-func CleanupOperation(ctx context.Context, dbOperation db.Operation, k8sOperation operation.Operation, operationNamespace string,
-	dbQueries db.ApplicationScopedQueries, gitopsEngineClient client.Client, log logr.Logger) error {
-
-	log = log.WithValues("operation", dbOperation.Operation_id, "namespace", operationNamespace)
-
-	// // Delete the database entry
-	// rowsDeleted, err := dbQueries.DeleteOperationById(ctx, dbOperation.Operation_id)
-	// if err != nil {
-	// 	return err
-	// }
-	// if rowsDeleted != 1 {
-	// 	log.V(sharedutil.LogLevel_Warn).Error(err, "unexpected number of operation rows deleted", "operation-id", dbOperation.Operation_id, "rows", rowsDeleted)
-	// }
-
-	log.V(sharedutil.LogLevel_Debug).Info("Deleting operation CR: " + k8sOperation.Name)
-
-	// Optional: Delete the Operation CR
-	if err := gitopsEngineClient.Delete(ctx, &k8sOperation); err != nil {
-		if !apierr.IsNotFound(err) {
-			// Log the error, but don't return it: it's the responsibility of the cluster agent to delete the operation cr
-			log.Error(err, "Unable to delete operation")
-		}
-	}
-	sharedutil.LogAPIResourceChangeEvent(k8sOperation.Namespace, k8sOperation.Name, k8sOperation, sharedutil.ResourceModified, log)
-
-	return nil
-
-}

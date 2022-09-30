80ef943d282b9a0f7d95d75b49f4631cb59c1584
Samyak Jain <samyak.jn11@gmail.com>
2022-10-01 00:33:49 +0530

    Setup script for e2e kcp ws based testing
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

---
+++ kcp/kcp-e2e/setup-ws-e2e.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -ex
+
+source ./kcp/utils.sh
+
+SERVICE_WS="gitops-service-provider"
+USER_WS="user-workspace"
+
+# Topology for the virtual workspace:
+# - user root
+#   - gitops-service-e2e-test
+#     - gitops-service-provider
+#     - user-workspace
+
+echo "-- Setup KCP virtual workspace for e2e testing initialised --"
+
+# Read the CPS KUBECONFIG path if it's not set already
+readKUBECONFIGPath
+
+echo "Entering the home workspace"
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace
+
+echo "Initializing gitops-service-e2e-test workspace"
+createAndEnterWorkspace "gitops-service-e2e-test"
+
+echo "Initializing service provider workspace"
+# switching to parent workspace will ensure the childrens are created properly
+kubectl kcp workspace use "gitops-service-e2e-test"
+createAndEnterWorkspace "${SERVICE_WS}"
+cp $KUBECONFIG /tmp/service-provider-workspace.yaml
+
+# Installing ArgoCD in service-provider workspace
+installArgoCD
+
+echo "Creating APIExports and APIResourceSchemas in workspace ${SERVICE_WS}"
+KUBECONFIG="${CPS_KUBECONFIG}" make apply-kcp-api-all
+
+# Copy the identity hash from the backend APIExport in the service workspace so we can reference it later in the appstudio APIBinding
+identityHash=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get apiexports.apis.kcp.dev gitopsrvc-backend-shared -o jsonpath='{.status.identityHash}')
+
+permissionToBindAPIExport
+
+echo "Initializing user workspace"
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace use "gitops-service-e2e-test"
+createAndEnterWorkspace "${USER_WS}"
+cp $KUBECONFIG /tmp/user-provider-workspace.yaml
+
+echo "Creating APIBindings in workspace ${USER_WS}"
+createAPIBinding gitopsrvc-backend-shared
+createAPIBinding gitopsrvc-appstudio-shared
+
+# Checking if the bindings are in Ready state
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl wait --for=condition=Ready apibindings/gitopsrvc-appstudio-shared
+KUBECONFIG="${CPS_KUBECONFIG}" kubectl wait --for=condition=Ready apibindings/gitopsrvc-backend-shared
+
+echo "-- Setup KCP virtual workspace for e2e testing successful --"

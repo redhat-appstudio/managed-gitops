ab5b724ccb39fd030dbca1176e62455aa30ca501
Samyak Jain <samyak.jn11@gmail.com>
2022-10-01 00:33:11 +0530

    Refractor code to increase reusability
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- kcp/utils.sh
+++ kcp/utils.sh
@@ -167,3 +167,80 @@ registerSyncTarget() {
     echo "Waiting for the SyncTarget resource to reach ready state"
     KUBECONFIG="${CPS_KUBECONFIG}" kubectl wait --for=condition=Ready=true synctarget/"${WORKLOAD_CLUSTER}" --timeout 3m
 }
+
+createAPIBinding() {
+    exportName=$1
+    KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws
+    url=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get workspace $SERVICE_WS -o jsonpath='{.status.URL}')
+    path=$(basename $url)
+    KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws use $USER_WS
+
+    permissionClaims='
+  permissionClaims:
+  - group: ""
+    resource: "secrets"
+    state: "Accepted"
+  - group: ""
+    resource: "namespaces"
+    state: "Accepted"'
+
+  if [ exportName == "gitopsrvc-appstudio-shared" ]; then
+    permissionClaims="${acceptedPermissionClaims}
+  - group: \"managed-gitops.redhat.com\"
+    resource: \"gitopsdeployments\"
+    state: \"Accepted\"
+    identityHash: ${identityHash}"
+  fi
+
+cat <<EOF | KUBECONFIG="${CPS_KUBECONFIG}" kubectl apply -f -
+apiVersion: apis.kcp.dev/v1alpha1
+kind: APIBinding
+metadata:
+  name: ${exportName}
+spec:
+${permissionClaims}
+  reference:
+    workspace:
+      path: ${path}
+      exportName: ${exportName}
+EOF
+}
+
+permissionToBindAPIExport() {
+  # Create permissions to bind APIExports. We need this workaround until KCP fixes the bug in their admission logic. Ref: https://github.com/kcp-dev/kcp/issues/1939  
+  KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws
+  bindingName=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get clusterrolebinding | grep $SERVICE_WS | awk '{print $1}')
+  userName=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get clusterrolebindings $bindingName -o jsonpath='{.subjects[0].name}')
+
+  KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws use $SERVICE_WS
+
+cat <<EOF | KUBECONFIG="${CPS_KUBECONFIG}" kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: bind-apiexport
+rules:
+- apiGroups:
+  - apis.kcp.dev
+  resourceNames:
+  - gitopsrvc-backend-shared
+  - gitopsrvc-appstudio-shared
+  resources:
+  - apiexports
+  verbs:
+  - bind
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: bind-apiexport
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: bind-apiexport
+subjects:
+- apiGroup: rbac.authorization.k8s.io
+  kind: User
+  name: "$userName"
+EOF
+}
--- kcp/virtual-workspace.sh
+++ kcp/virtual-workspace.sh
@@ -30,84 +30,12 @@ KUBECONFIG="${CPS_KUBECONFIG}" make apply-kcp-api-all
 # Copy the identity hash from the backend APIExport in the service workspace so we can reference it later in the appstudio APIBinding
 identityHash=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get apiexports.apis.kcp.dev gitopsrvc-backend-shared -o jsonpath='{.status.identityHash}')
 
-# Create permissions to bind APIExports. We need this workaround until KCP fixes the bug in their admission logic. Ref: https://github.com/kcp-dev/kcp/issues/1939  
-KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws
-bindingName=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get clusterrolebinding | grep $SERVICE_WS | awk '{print $1}')
-userName=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get clusterrolebindings $bindingName -o jsonpath='{.subjects[0].name}')
-
-KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws use $SERVICE_WS
-cat <<EOF | KUBECONFIG="${CPS_KUBECONFIG}" kubectl apply -f -
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: bind-apiexport
-rules:
-- apiGroups:
-  - apis.kcp.dev
-  resourceNames:
-  - gitopsrvc-backend-shared
-  - gitopsrvc-appstudio-shared
-  resources:
-  - apiexports
-  verbs:
-  - bind
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: bind-apiexport
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: bind-apiexport
-subjects:
-- apiGroup: rbac.authorization.k8s.io
-  kind: User
-  name: "$userName"
-EOF
+permissionToBindAPIExport 
 
 echo "Initializing user workspace"
 createAndEnterWorkspace "$USER_WS"
 
-createAPIBinding() {
-    exportName=$1
-    KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws
-    url=$(KUBECONFIG="${CPS_KUBECONFIG}" kubectl get workspace $SERVICE_WS -o jsonpath='{.status.URL}')
-    path=$(basename $url)
-    KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp ws use $USER_WS
-
-    permissionClaims='
-  permissionClaims:
-  - group: ""
-    resource: "secrets"
-    state: "Accepted"
-  - group: ""
-    resource: "namespaces"
-    state: "Accepted"'
-
-  if [ exportName == "gitopsrvc-appstudio-shared" ]; then
-    permissionClaims="${acceptedPermissionClaims}
-  - group: \"managed-gitops.redhat.com\"
-    resource: \"gitopsdeployments\"
-    state: \"Accepted\"
-    identityHash: ${identityHash}"
-  fi
-
-cat <<EOF | KUBECONFIG="${CPS_KUBECONFIG}" kubectl apply -f -
-apiVersion: apis.kcp.dev/v1alpha1
-kind: APIBinding
-metadata:
-  name: ${exportName}
-spec:
-${permissionClaims}
-  reference:
-    workspace:
-      path: ${path}
-      exportName: ${exportName}
-EOF
-}
-
-echo "Creating APIBindings in workspace $USER_wS"
+echo "Creating APIBindings in workspace $USER_WS"
 createAPIBinding gitopsrvc-backend-shared
 createAPIBinding gitopsrvc-appstudio-shared
 

881a6b3e09a41d1020c2aa712765ae60bf5c9353
Samyak Jain <samyak.jn11@gmail.com>
2022-10-26 17:22:55 +0530

    Export serviceWS and userWS kubeconfig to respective variables
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- kcp/kcp-e2e/setup-ws-e2e.sh
+++ kcp/kcp-e2e/setup-ws-e2e.sh
@@ -35,6 +35,8 @@ echo "Initializing service provider workspace"
 KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace
 KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace use "${PARENT_E2E_WS}"
 createAndEnterWorkspace "${SERVICE_WS}"
+KUBECONFIG="${CPS_KUBECONFIG}"  kubectl config view > /tmp/serviceWs.kubeconfig
+export SERVICE_PROVIDER_KUBECONFIG=/tmp/serviceWs.kubeconfig
 
 KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workload sync "service-ws-test-cluster" --resources "services,statefulsets.apps,deployments.apps,routes.route.openshift.io" --syncer-image ghcr.io/kcp-dev/kcp/syncer:v0.9.0 --output-file ./syncer-servicews.yaml --namespace kcp-syncer
 kubectl apply -f ./syncer-servicews.yaml
@@ -53,7 +55,8 @@ echo "Initializing user workspace"
 KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace
 KUBECONFIG="${CPS_KUBECONFIG}" kubectl kcp workspace use "${PARENT_E2E_WS}"
 createAndEnterWorkspace "${USER_WS}"
-
+KUBECONFIG="${CPS_KUBECONFIG}"  kubectl config view > /tmp/userWs.kubeconfig
+export USER_KUBECONFIG=/tmp/userWs.kubeconfig
 
 echo "Creating APIBindings in workspace ${USER_WS}"
 createAPIBinding gitopsrvc-backend-shared "${PARENT_E2E_WS}" "${SERVICE_WS}"

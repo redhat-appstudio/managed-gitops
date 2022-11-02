15b04b54af3ee563c0a1174551fb7ab2e57bb51a
Panagiotis Georgiadis <pgeorgia@redhat.com>
2022-11-02 13:32:45 +0100

    Update load-test ArgoCD v2.5.1

--- utilities/load-test/main.go
+++ utilities/load-test/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	utils "github.com/redhat-appstudio/managed-gitops/utilities/load-test/loadtest"
 )
 
+const ARGO_CD_VERSION = "v2.5.1"
+
 func main() {
+
+	manifest := fmt.Sprintf("https://raw.githubusercontent.com/argoproj/argo-cd/%s/manifests/install.yaml", ARGO_CD_VERSION)
 	// Running a kubectl apply command by passing namespace and URL for the manifests yaml(s)
-	utils.KubectlApply("argocd", "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml")
+	utils.KubectlApply("argocd", manifest)
 }

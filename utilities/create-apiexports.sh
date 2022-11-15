f6625373d3488a0d2699427963251085d48882f7
Panagiotis Georgiadis <pgeorgia@redhat.com>
2022-11-16 00:26:56 +0100

    Add APPLICATION_API_COMMIT var for Makefile

--- utilities/create-apiexports.sh
+++ utilities/create-apiexports.sh
@@ -2,6 +2,14 @@
 
 set -xe
 
+APPLICATION_API_COMMIT="$1"
+
+# Check if APPLICATION_API_COMMIT is set
+if [ -z "$APPLICATION_API_COMMIT" ]; then
+    echo "APPLICATION_API_COMMIT is not set"
+    exit 1
+fi
+
 kubectl apply -k backend-shared/config/kcp
 
 # Wait for the Virtual Workspace URL and Identity hash to be generated 
@@ -9,10 +17,10 @@ kubectl wait --for=condition=VirtualWorkspaceURLsReady apiexports/gitopsrvc-back
 
 identityHash=$(kubectl get apiexports.apis.kcp.dev gitopsrvc-backend-shared -o jsonpath='{.status.identityHash}')
 
-kubectl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/main/config/kcp/apibinding.yaml
-kubectl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/main/config/kcp/apiexport.yaml
-kubextl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/main/config/kcp/apiresourceschema.yaml
-kubectl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/main/config/kcp/kustomization.yaml
+kubectl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/${APPLICATION_API_COMMIT}/config/kcp/apibinding.yaml
+kubectl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/${APPLICATION_API_COMMIT}/config/kcp/apiexport.yaml
+kubextl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/${APPLICATION_API_COMMIT}/config/kcp/apiresourceschema.yaml
+kubectl apply -f https://raw.githubusercontent.com/redhat-appstudio/application-api/${APPLICATION_API_COMMIT}/config/kcp/kustomization.yaml
 
 
 # Add the Identity hash to the Appstudio APIExport in order to claim GitOpsDeployment from Backend APIExport

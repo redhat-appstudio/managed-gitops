68e27e93e0bada602e19076f1e401fb90f7272d7
Flavius Lacatusu <flacatus@redhat.com>
2022-11-30 12:55:25 +0100

    Add secrets resources to the appstudio controller

--- appstudio-controller/controllers/appstudio.redhat.com/environment_controller.go
+++ appstudio-controller/controllers/appstudio.redhat.com/environment_controller.go
@@ -44,6 +44,7 @@ type EnvironmentReconciler struct {
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=environments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=environments/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=environments/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.

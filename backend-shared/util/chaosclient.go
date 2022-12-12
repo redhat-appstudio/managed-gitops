a978f2d460e25f33eb3a9eb56a36038a171c7780
Jonathan West <jonwest@redhat.com>
2022-12-12 17:00:55 -0500

    Add output of injected errors, add explanatory comments

--- backend-shared/util/chaosclient.go
+++ backend-shared/util/chaosclient.go
@@ -16,6 +16,20 @@ var (
 	_ client.Client = &ChaosClient{}
 )
 
+// ChaosClient is a k8s Client that optionally simulates an unreliable connections/cluster.
+//
+// This unreliable client will be enabled by an environment variable, which we can/will enable when running E2E tests.
+// By running E2E tests with this environment variable set, this will allow us to ensure that our product works even
+// when running in less reliable conditions.
+//
+// The unreliable client will randomly fail a certain percent, and that % is controllable via environment variable.
+//
+// To enable it, set the following environment varaibles before running the GitOps Service controllers.
+// For example:
+// - ENABLE_UNRELIABLE_CLIENT=true
+// - UNRELIABLE_CLIENT_FAILURE_RATE=10
+//
+// This example will simulate a 10% failure rate. (e.g. 10% of K8s requests will fail, throughout the code)
 type ChaosClient struct {
 	InnerClient client.Client
 }
@@ -30,7 +44,8 @@ func isEnvExist(key string) bool {
 
 // IfEnabledSimulateUnreliableClient will wrap a regular client.Client, and randomly simulate connection issues.
 func IfEnabledSimulateUnreliableClient(kClient client.Client) client.Client {
-	// set environment variable(ENABLE_UNRELIABLE_CLIENT) in your terminal before running e2e tests
+
+	// set environment variable(ENABLE_UNRELIABLE_CLIENT) in your UNRELIABLE_CLIENT_FAILURE_RATEterminal before running e2e tests
 	if os.Getenv("ENABLE_UNRELIABLE_CLIENT") == "true" {
 		return &ChaosClient{InnerClient: kClient}
 	} else {
@@ -43,11 +58,11 @@ func IfEnabledSimulateUnreliableClient(kClient client.Client) client.Client {
 // returned by the Server.
 func (pc *ChaosClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object) error {
 
-	res := pc.InnerClient.Get(ctx, key, obj)
-
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("Get", obj); err != nil {
 		return err
 	}
+
+	res := pc.InnerClient.Get(ctx, key, obj)
 
 	return res
 }
@@ -56,33 +71,36 @@ func (pc *ChaosClient) Get(ctx context.Context, key client.ObjectKey, obj client
 // successful call, Items field in the list will be populated with the
 // result returned from the server.
 func (pc *ChaosClient) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
-	res := pc.InnerClient.List(ctx, list, opts...)
 
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("List", nil); err != nil {
 		return err
 	}
+
+	res := pc.InnerClient.List(ctx, list, opts...)
 
 	return res
 }
 
 // Create saves the object obj in the Kubernetes cluster.
 func (pc *ChaosClient) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
-	res := pc.InnerClient.Create(ctx, obj, opts...)
 
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("Create", obj); err != nil {
 		return err
 	}
+
+	res := pc.InnerClient.Create(ctx, obj, opts...)
 
 	return res
 }
 
 // Delete deletes the given obj from Kubernetes cluster.
 func (pc *ChaosClient) Delete(ctx context.Context, obj client.Object, opts ...client.DeleteOption) error {
-	res := pc.InnerClient.Delete(ctx, obj, opts...)
 
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("Delete", obj); err != nil {
 		return err
 	}
+
+	res := pc.InnerClient.Delete(ctx, obj, opts...)
 
 	return res
 }
@@ -90,11 +108,12 @@ func (pc *ChaosClient) Delete(ctx context.Context, obj client.Object, opts ...cl
 // Update updates the given obj in the Kubernetes cluster. obj must be a
 // struct pointer so that obj can be updated with the content returned by the Server.
 func (pc *ChaosClient) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
-	res := pc.InnerClient.Update(ctx, obj, opts...)
 
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("Update", obj); err != nil {
 		return err
 	}
+
+	res := pc.InnerClient.Update(ctx, obj, opts...)
 
 	return res
 }
@@ -102,22 +121,24 @@ func (pc *ChaosClient) Update(ctx context.Context, obj client.Object, opts ...cl
 // Patch patches the given obj in the Kubernetes cluster. obj must be a
 // struct pointer so that obj can be updated with the content returned by the Server.
 func (pc *ChaosClient) Patch(ctx context.Context, obj client.Object, patch client.Patch, opts ...client.PatchOption) error {
-	res := pc.InnerClient.Patch(ctx, obj, patch, opts...)
 
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("Patch", obj); err != nil {
 		return err
 	}
+
+	res := pc.InnerClient.Patch(ctx, obj, patch, opts...)
 
 	return res
 }
 
 // DeleteAllOf deletes all objects of the given type matching the given options.
 func (pc *ChaosClient) DeleteAllOf(ctx context.Context, obj client.Object, opts ...client.DeleteAllOfOption) error {
-	res := pc.InnerClient.DeleteAllOf(ctx, obj, opts...)
 
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("DeleteAllOf", obj); err != nil {
 		return err
 	}
+
+	res := pc.InnerClient.DeleteAllOf(ctx, obj, opts...)
 
 	return res
 }
@@ -151,11 +172,11 @@ type ChaosClientStatusWrapper struct {
 // with the content returned by the Server.
 func (pcsw *ChaosClientStatusWrapper) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
 
-	res := (*pcsw.innerWriter).Update(ctx, obj, opts...)
-
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("Status Update", obj); err != nil {
 		return err
 	}
+
+	res := (*pcsw.innerWriter).Update(ctx, obj, opts...)
 
 	return res
 
@@ -166,7 +187,7 @@ func (pcsw *ChaosClientStatusWrapper) Update(ctx context.Context, obj client.Obj
 // Server.
 func (pcsw *ChaosClientStatusWrapper) Patch(ctx context.Context, obj client.Object, patch client.Patch, opts ...client.PatchOption) error {
 
-	if err := shouldSimulateFailure(); err != nil {
+	if err := shouldSimulateFailure("Status Patch", obj); err != nil {
 		return err
 	}
 
@@ -175,7 +196,7 @@ func (pcsw *ChaosClientStatusWrapper) Patch(ctx context.Context, obj client.Obje
 	return res
 }
 
-func shouldSimulateFailure() error {
+func shouldSimulateFailure(apiType string, obj client.Object) error {
 
 	if !isEnvExist("UNRELIABLE_CLIENT_FAILURE_RATE") {
 		return nil
@@ -189,12 +210,13 @@ func shouldSimulateFailure() error {
 	}
 
 	// Convert it to a decimal point, e.g. 50% to 0.5
-	unreliableClientFailureRateValue = unreliableClientFailureRateValue / 100
+	unreliableClientFailureRateValueDouble := (float64)(unreliableClientFailureRateValue) / (float64)(100)
 
 	// return an error randomly x% of the time (using math/rand package)
 	// #nosec G404 -- not used for cryptographic purposes
-	if rand.Float64() < float64(unreliableClientFailureRateValue) {
-		return fmt.Errorf("simulated K8s error")
+	if rand.Float64() < float64(unreliableClientFailureRateValueDouble) {
+		fmt.Println("chaosclient.go - Simulated error:", apiType, obj)
+		return fmt.Errorf("chaosclient.go - simulated K8s error: %s %v", apiType, obj)
 	}
 
 	return nil

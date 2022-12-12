43aaff5fa8a299d936345c7af974aff96b944fa6
Jonathan West <jonwest@redhat.com>
2022-12-12 15:15:11 -0500

    Add a new unit test to verify the application event loop rejects new work

--- backend/eventloop/application_event_loop/application_event_loop_test.go
+++ backend/eventloop/application_event_loop/application_event_loop_test.go
@@ -2,6 +2,8 @@ package application_event_loop
 
 import (
 	"context"
+	"sync"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -15,7 +17,7 @@ var _ = Describe("ApplicationEventLoop Test", func() {
 
 	Context("Application Event Loop test", func() {
 
-		It("should forward messages received on the input channel, to the runner", func() {
+		It("should forward messages received on the input channel, to the runner, without a response channel", func() {
 
 			mockApplicationEventLoopRunnerFactory := mockApplicationEventLoopRunnerFactory{
 				mockChannel: make(chan *eventlooptypes.EventLoopEvent),
@@ -44,6 +46,7 @@ var _ = Describe("ApplicationEventLoop Test", func() {
 					},
 					ShutdownSignalled: false,
 				},
+				ResponseChan: nil, // no response channel needed
 			}
 
 			outputEvent := <-mockApplicationEventLoopRunnerFactory.mockChannel
@@ -53,6 +56,117 @@ var _ = Describe("ApplicationEventLoop Test", func() {
 			Expect(outputEvent.ReqResource).To(Equal(outputEvent.ReqResource))
 			Expect(outputEvent.WorkspaceID).To(Equal(outputEvent.WorkspaceID))
 
+		})
+	})
+
+	Context("Simulate a deleted GitOpsDeployment", Ordered, func() {
+
+		const (
+			gitopsDeplName      = "my-gitops-depl"
+			gitopsDeplNamespace = "my-namespace"
+			namespaceUID        = "my-namespace-uid"
+		)
+
+		aeqlParam := ApplicationEventQueueLoop{
+			GitopsDeploymentName:      gitopsDeplName,
+			GitopsDeploymentNamespace: gitopsDeplNamespace,
+			WorkspaceID:               namespaceUID,
+			SharedResourceEventLoop:   nil,
+			VwsAPIExportName:          "gitops-api",
+			InputChan:                 make(chan RequestMessage),
+		}
+
+		event := &eventlooptypes.EventLoopEvent{
+			EventType:   eventlooptypes.DeploymentModified,
+			Request:     reconcile.Request{NamespacedName: types.NamespacedName{Namespace: gitopsDeplNamespace, Name: gitopsDeplName}},
+			Client:      nil,
+			ReqResource: eventlooptypes.GitOpsDeploymentTypeName,
+			WorkspaceID: namespaceUID,
+		}
+
+		It("should start a runner based on incoming GitOpsDeployment request ", func() {
+			mockApplicationEventLoopRunnerFactory := mockApplicationEventLoopRunnerFactory{
+				mockChannel: make(chan *eventlooptypes.EventLoopEvent),
+			}
+
+			startApplicationEventQueueLoopWithFactory(context.Background(), aeqlParam, &mockApplicationEventLoopRunnerFactory)
+
+			By("sending message to the application event loop, informing it of a new GitOpsDeployment")
+			responseChan := make(chan ResponseMessage)
+			aeqlParam.InputChan <- RequestMessage{
+				Message: eventlooptypes.EventLoopMessage{
+					MessageType:       eventlooptypes.ApplicationEventLoopMessageType_Event,
+					Event:             event,
+					ShutdownSignalled: false,
+				},
+				ResponseChan: responseChan,
+			}
+
+			By("By reading the response message from the application event loop")
+			response := <-responseChan
+			Expect(response.RequestAccepted).To(BeTrue())
+
+			outputEvent := <-mockApplicationEventLoopRunnerFactory.mockChannel
+			Expect(outputEvent).NotTo(BeNil(), "we should have received the event on a runner")
+			Expect(outputEvent.EventType).To(Equal(outputEvent.EventType))
+			Expect(outputEvent.Request).To(Equal(outputEvent.Request))
+			Expect(outputEvent.ReqResource).To(Equal(outputEvent.ReqResource))
+			Expect(outputEvent.WorkspaceID).To(Equal(outputEvent.WorkspaceID))
+
+		})
+
+		It("should shutdown the runner based on work complete, and then verify the event loop rejects additional work", func() {
+
+			By("sending message to application event loop that the runner is complete")
+			aeqlParam.InputChan <- RequestMessage{
+				Message: eventlooptypes.EventLoopMessage{
+					MessageType:       eventlooptypes.ApplicationEventLoopMessageType_WorkComplete,
+					Event:             event,
+					ShutdownSignalled: true,
+				},
+				ResponseChan: nil,
+			}
+
+			By("by attempting to send another request to the application event loop")
+			responseChan := make(chan ResponseMessage)
+			aeqlParam.InputChan <- RequestMessage{
+				Message: eventlooptypes.EventLoopMessage{
+					MessageType:       eventlooptypes.ApplicationEventLoopMessageType_Event,
+					Event:             event,
+					ShutdownSignalled: false,
+				},
+				ResponseChan: responseChan,
+			}
+
+			response := <-responseChan
+			Expect(response.RequestAccepted).To(BeFalse(), "the second request to the event loop should be rejected, as "+
+				"the application event loop has terminate.")
+
+			By("attempting to send another request to the application event loop")
+			mutex := sync.Mutex{}
+			responseReceived := false
+			go func() {
+				responseChan = make(chan ResponseMessage)
+				aeqlParam.InputChan <- RequestMessage{
+					Message: eventlooptypes.EventLoopMessage{
+						MessageType:       eventlooptypes.ApplicationEventLoopMessageType_Event,
+						Event:             event,
+						ShutdownSignalled: false,
+					},
+					ResponseChan: nil,
+				}
+				mutex.Lock()
+				defer mutex.Unlock()
+				responseReceived = true
+
+			}()
+
+			Consistently(func() bool {
+				mutex.Lock()
+				defer mutex.Unlock()
+				return responseReceived
+			}).WithTimeout(time.Second).WithPolling(time.Millisecond*10).Should(BeFalse(),
+				"we should not receive a response, because the event loop goroutine has terminated.")
 		})
 	})
 

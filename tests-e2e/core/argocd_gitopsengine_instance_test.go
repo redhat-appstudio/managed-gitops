ac2916b07814a671ae88784b559ebbc34c29808f
Jonathan West <jonwest@redhat.com>
2023-01-17 17:19:27 -0500

    Remove UUID from namespace creation

--- tests-e2e/core/argocd_gitopsengine_instance_test.go
+++ tests-e2e/core/argocd_gitopsengine_instance_test.go
@@ -12,7 +12,6 @@ import (
 	apps "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/uuid"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -59,9 +58,7 @@ var _ = Describe("ArgoCD instance via GitOpsEngineInstance Operations Test", fun
 			newArgoCDNamespace := &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: newArgoCDNamespaceName,
-					UID:  uuid.NewUUID(),
 				},
-				Spec: corev1.NamespaceSpec{},
 			}
 
 			err = k8sClient.Create(ctx, newArgoCDNamespace)

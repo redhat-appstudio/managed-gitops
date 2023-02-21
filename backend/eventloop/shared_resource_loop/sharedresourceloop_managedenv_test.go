3ab821e40f37adc95ee609e3002153b82fcbedd3
Raghavi Shirur <raghaviss11@gmail.com>
2023-02-21 11:32:50 +0530

    Added kubeconfig constant in backend

--- backend/eventloop/shared_resource_loop/sharedresourceloop_managedenv_test.go
+++ backend/eventloop/shared_resource_loop/sharedresourceloop_managedenv_test.go
@@ -838,7 +838,7 @@ func buildManagedEnvironmentForSRL() (managedgitopsv1alpha1.GitOpsDeploymentMana
 		},
 		Type: sharedutil.ManagedEnvironmentSecretType,
 		Data: map[string][]byte{
-			"kubeconfig": ([]byte)(kubeConfigContents),
+			Kubeconfig_key: ([]byte)(kubeConfigContents),
 		},
 	}
 

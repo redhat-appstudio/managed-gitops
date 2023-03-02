bec36b5803f56de8866bac9d70093316eef7c47d
Chetan Banavikalmutt <chetanrns1997@gmail.com>
2023-03-02 10:59:29 +0530

    Implement basic workflow for binding controller

---
+++ appstudio-controller/controllers/appstudio.redhat.com/deploymenttargetbinder_controller_test.go
@@ -0,0 +1,14 @@
+package appstudioredhatcom
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test DeploymentTargetClaimBindingController", func() {
+	Context("", func() {
+		It("", func() {
+
+		})
+	})
+})

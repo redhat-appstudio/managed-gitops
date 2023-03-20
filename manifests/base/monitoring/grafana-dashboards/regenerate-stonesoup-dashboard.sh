3dd994b367ce6909926a4d2ffc38f130333518b7
John Pitman <jpitman@redhat.com>
2023-03-20 07:19:28 -0400

    Add Argo CD Grafana dashboard for the GitOps Service instance (#374)
    Signed-off-by: John Pitman <jpitman@redhat.com>

--- manifests/base/monitoring/grafana-dashboards/regenerate-stonesoup-dashboard.sh
+++ manifests/base/monitoring/grafana-dashboards/regenerate-stonesoup-dashboard.sh
@@ -3,8 +3,9 @@
 # See README.md for the purpose of this script
 
 cp managed-gitops/gitops-dashboard.json stonesoup/gitops-dashboard.json
+cp managed-gitops/gitops-argocd-dashboard.json stonesoup/gitops-argocd-dashboard.json
 
 sed -i.bak -e 's/PBFA97CFB590B2093/PF224BEF3374A25F8/g' stonesoup/gitops-dashboard.json
+sed -i.bak -e 's/PBFA97CFB590B2093/PF224BEF3374A25F8/g' stonesoup/gitops-argocd-dashboard.json
 
 rm stonesoup/*.bak
-

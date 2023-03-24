0518d144269477c2215a1d993339fa93d1d85dfa
anandf <anandfrancis.joseph@gmail.com>
2023-03-24 15:58:07 +0530

    Conditionally creating subscription
    Signed-off-by: anandf <anandfrancis.joseph@gmail.com>

--- manifests/scripts/openshift-argo-deploy/deploy.sh
+++ manifests/scripts/openshift-argo-deploy/deploy.sh
@@ -7,22 +7,29 @@ SCRIPTPATH="$(
 
 # A simple script for setting up Argo CD via OpenShift GitOps, on Argo CD
 
-kubectl apply -f $SCRIPTPATH/openshift-gitops-subscription.yaml
+SKIP_SUBSCRIPTION_INSTALL=${SKIP_SUBSCRIPTION_INSTALL:-"false"}
 
-kubectl create namespace gitops-service-argocd 2> /dev/null || true
-echo -n "Waiting for namespace to exist: "
-while ! kubectl get namespace gitops-service-argocd &> /dev/null ; do
-  echo -n .
-  sleep 1
-done
-echo "OK"
+if [[ "$SKIP_SUBSCRIPTION_INSTALL" == "true" ]]; then
+  echo "Skipping subscription install"
+else
+  echo "Installing subscription"
+  kubectl apply -f $SCRIPTPATH/openshift-gitops-subscription.yaml
 
-echo -n "Checking for gitops operator controller pod to be created and running before proceeding with the next step:"
-while ! kubectl get pods -n openshift-operators | grep gitops-operator-controller-manager | grep Running &> /dev/null ; do
-  echo -n .
-  sleep 1
-done
-echo "OK"
+  kubectl create namespace gitops-service-argocd 2> /dev/null || true
+  echo -n "Waiting for namespace to exist: "
+  while ! kubectl get namespace gitops-service-argocd &> /dev/null ; do
+    echo -n .
+    sleep 1
+  done
+  echo "OK"
+
+  echo -n "Checking for gitops operator controller pod to be created and running before proceeding with the next step:"
+  while ! kubectl get pods -n openshift-operators | grep gitops-operator-controller-manager | grep Running &> /dev/null ; do
+    echo -n .
+    sleep 1
+  done
+  echo "OK"
+fi
 
 echo "Installing Argo CD into gitops-service-argocd"
 kustomize build $SCRIPTPATH/../../base/gitops-service-argocd/overlays/test-e2e | kubectl apply -f -

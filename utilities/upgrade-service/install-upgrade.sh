e7df04650f916d8024663abfe5319ccf5ae6f940
Anand Francis Joseph <anjoseph@redhat.com>
2023-04-04 07:14:44 +0530

    Addressed review comments to add more logging and customizable revision
    Signed-off-by: Anand Francis Joseph <anjoseph@redhat.com>

--- utilities/upgrade-service/install-upgrade.sh
+++ utilities/upgrade-service/install-upgrade.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(
-  cd "$(dirname "$0")" >/dev/null
-  pwd
-)"
+KUSTOMIZE_VERSION=${KUSTOMIZE_VERSION:-"v4.5.7"}
+KUBECTL_VERSION=${KUBECTL_VERSION:-"v1.26.0"}
+
+# Revision of the kubernetes manifests to be used for the installation
+GIT_REVISION=${GIT_REVISION:-"main"}
 
 # deletes the temp directory
 function cleanup() {
   rm -rf "${TEMP_DIR}"
-  echo "Deleted temp working directory $WORK_DIR"
+  echo "Deleted temp working directory ${TEMP_DIR}"
 }
 
 # installs the stable version kustomize binary if not found in PATH
 function install_kustomize() {
   if [[ -z "${KUSTOMIZE}" ]]; then
-    wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.5.7/kustomize_v4.5.7_$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m |sed s/aarch64/arm64/ | sed s/x86_64/amd64/).tar.gz -O ${TEMP_DIR}/kustomize.tar.gz
+    echo "[INFO] kustomize binary not found in \$PATH, installing kustomize-${KUSTOMIZE_VERSION} in ${TEMP_DIR}"
+    wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m |sed s/aarch64/arm64/ | sed s/x86_64/amd64/).tar.gz -O ${TEMP_DIR}/kustomize.tar.gz
     tar zxvf ${TEMP_DIR}/kustomize.tar.gz -C ${TEMP_DIR}
     KUSTOMIZE=${TEMP_DIR}/kustomize
     chmod +x ${TEMP_DIR}/kustomize
@@ -24,7 +26,8 @@ function install_kustomize() {
 # installs the stable version of kubectl binary if not found in PATH
 function install_kubectl() {
   if [[ -z "${KUBECTL}" ]]; then
-    wget https://dl.k8s.io/release/v1.26.0/bin/$(uname | tr '[:upper:]' '[:lower:]')/$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)/kubectl -O ${TEMP_DIR}/kubectl
+    echo "[INFO] kubectl binary not found in \$PATH, installing kubectl-${KUBECTL_VERSION} in ${TEMP_DIR}"
+    wget https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/$(uname | tr '[:upper:]' '[:lower:]')/$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)/kubectl -O ${TEMP_DIR}/kubectl
     KUBECTL=${TEMP_DIR}/kubectl
     chmod +x ${TEMP_DIR}/kubectl
   fi
@@ -69,23 +72,34 @@ rollback() {
 
 # creates a kustomization.yaml file in the temp directory pointing to the manifests available in the upstream repo.
 function create_kustomization_init_file() {
+  echo "[INFO] Creating kustomization.yaml file using manifests from revision ${GIT_REVISION}"
   echo "apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
-  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/crd/overlays/local-dev?ref=$GIT_REVISION
-  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/gitops-namespace?ref=$GIT_REVISION
-  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/gitops-service-argocd/base?ref=$GIT_REVISION
-  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/postgresql-staging?ref=$GIT_REVISION
-  - https://github.com/redhat-appstudio/managed-gitops/appstudio-controller/config/default?ref=$GIT_REVISION
-  - https://github.com/redhat-appstudio/managed-gitops/backend/config/default?ref=$GIT_REVISION
-  - https://github.com/redhat-appstudio/managed-gitops/cluster-agent/config/default?ref=$GIT_REVISION" > ${TEMP_DIR}/kustomization.yaml
+  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/crd/overlays/local-dev?ref=${GIT_REVISION}
+  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/gitops-namespace?ref=${GIT_REVISION}
+  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/gitops-service-argocd/base?ref=${GIT_REVISION}
+  - https://github.com/redhat-appstudio/managed-gitops/manifests/base/postgresql-staging?ref=${GIT_REVISION}
+  - https://github.com/redhat-appstudio/managed-gitops/appstudio-controller/config/default?ref=${GIT_REVISION}
+  - https://github.com/redhat-appstudio/managed-gitops/backend/config/default?ref=${GIT_REVISION}
+  - https://github.com/redhat-appstudio/managed-gitops/cluster-agent/config/default?ref=${GIT_REVISION}" > ${TEMP_DIR}/kustomization.yaml
+  cat ${TEMP_DIR}/kustomization.yaml
+}
+
+# Auto-generate DB Secret if not already present
+function generate_postgresql_secret {
+  echo "Generating secret for postgresql-password"
+  if ! kubectl get secret -n gitops gitops-postgresql-staging &>/dev/null; then
+    kubectl create secret generic gitops-postgresql-staging \
+    --namespace=gitops \
+    --from-literal=postgresql-password=$(openssl rand -base64 20)
+  fi
 }
 
 # Code execution starts here
 # create a temporary directory and do all the operations inside the directory.
-GIT_REVISION=main
 TEMP_DIR=$(mktemp -d -t managed-gitops-install-XXXXXXX)
-echo "Using temp directory $TEMP_DIR"
+echo "Using temp directory ${TEMP_DIR}"
 # cleanup the temporary directory irrespective of whether the script ran successfully or failed with an error.
 trap cleanup EXIT
 
@@ -114,7 +128,7 @@ fi
 
 echo "IMAGE $IMG";
 
-PREV_IMAGE=$(kubectl get deploy/gitops-appstudio-service-controller-manager -n gitops -o jsonpath='{..image}');
+PREV_IMAGE=$(${KUBECTL} get deploy/gitops-appstudio-service-controller-manager -n gitops -o jsonpath='{..image}');
 echo "PREV IMAGE : $PREV_IMAGE";
 
 if [ "$PREV_IMAGE" = "$IMG" ]; then
@@ -128,10 +142,11 @@ echo "Upgrading from $PREV_IMAGE to $IMG";
 create_kustomization_init_file
 
 # Set the right container image and apply the manifests
-${KUSTOMIZE} build ${TEMP_DIR} |  COMMON_IMAGE=${IMG} envsubst | kubectl apply -f -
+echo "[INFO] Applying the manifests generated using kustomize"
+${KUSTOMIZE} build ${TEMP_DIR} |  COMMON_IMAGE=${IMG} envsubst | ${KUBECTL} apply -f -
 
-# Create Postgresql DB secret
-wget -O - https://raw.githubusercontent.com/anandf/managed-gitops/main/manifests/scripts/generate-postgresql-secret.sh | bash
+# Create Postgresql DB password secret
+generate_postgresql_secret
 
 echo 'Wait until pods are running';
 

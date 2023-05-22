a97746a86fddbbb7c3ebb79bdb03c495b5f63fea
Jonathan West <jonwest@redhat.com>
2023-05-22 17:52:47 -0400

    Remove unused code and update codecov.yml

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -14,32 +14,6 @@ func RouteInit() *http.Server {
 	wsContainer := restful.NewContainer()
 	wsContainer.Router(restful.CurlyRouter{})
 
-	// // Registering operation resource to the wsContainer
-	// o := operations.OperationResource{Operations: map[string]operations.Operation{}}
-	// o.Register(wsContainer)
-
-	// // Registering application resource to the wsContainer
-	// a := application.ApplicationResource{Applications: map[string]application.Application{}}
-	// a.Register(wsContainer)
-
-	// // Registering managed environment resource to the wsContainer
-	// ws := new(restful.WebService)
-	// ws.
-	// 	Path("/api/v1/managedenvironment").
-	// 	Consumes(restful.MIME_JSON).
-	// 	Produces(restful.MIME_JSON)
-
-	// ws.Route(ws.GET("").To(managedenvironment.HandleListManagedEnvironments).
-	// 	Returns(200, "OK", "List of Managed Clusters").
-	// 	Returns(404, "Error Occured, Not Found", nil))
-
-	// ws.Route(ws.POST("").To(managedenvironment.HandlePostManagedEnvironment).
-	// 	Returns(200, "OK", "URL for the WebUI").
-	// 	Returns(404, "Error Occured, Not Found", nil))
-
-	// ws.Route(ws.GET("/{managedenv-id}").To(managedenvironment.HandleGetASpecificManagementEnvironment))
-	// wsContainer.Add(ws)
-
 	webhookR := new(restful.WebService)
 	webhookR.
 		Path("/api/v1/webhookevent").

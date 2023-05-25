12017971f9db39dce36542df74fcc5d591d9dd6e
Jonathan West <jonwest@redhat.com>
2023-05-25 15:03:24 -0400

    Remove old 'manifests-v1/' folder

--- manifests-v1/database-init/regenerate-dbschema-config-map.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd kustomize
-
-kustomize build --load-restrictor LoadRestrictionsNone > ../dbschema-config-map.yaml
-
--- manifests-v1/postgresql-staging/addProtocol.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# KCP till release-0.7 has no knowledge of protocol by default, and hence we need to
-# add these protocols in our yamls explicitly. Here, we are defining explicitly TCP.
-
-yq  e -i 'select(.kind == "Service").spec.ports[] += {"protocol": "TCP"}' postgresql-staging.yaml
-yq  e -i 'select(.kind == "StatefulSet").spec.template.spec.containers[].ports[] += {"protocol": "TCP"}' postgresql-staging.yaml

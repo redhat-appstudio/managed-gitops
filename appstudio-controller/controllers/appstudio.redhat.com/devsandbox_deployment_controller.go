d3caa59f457d9189eadf87ad6e2dfad4b3b2aaf4
Jonathan West <jonwest@redhat.com>
2023-08-28 21:26:17 -0400

    RHTAPBUGS-735: clean up orphaned SpaceRequests, when DTC is gone

--- appstudio-controller/controllers/appstudio.redhat.com/devsandbox_deployment_controller.go
+++ appstudio-controller/controllers/appstudio.redhat.com/devsandbox_deployment_controller.go
@@ -24,17 +24,16 @@ import (
 	"strings"
 
 	codereadytoolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/toolchain-common/pkg/condition"
 	"github.com/go-logr/logr"
 	applicationv1alpha1 "github.com/redhat-appstudio/application-api/api/v1alpha1"
 	logutil "github.com/redhat-appstudio/managed-gitops/backend-shared/util/log"
 	apierr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 // DevsandboxDeploymentReconciler reconciles a SpaceRequest object
@@ -60,9 +59,6 @@ type DevsandboxDeploymentReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *DevsandboxDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	var dt *applicationv1alpha1.DeploymentTarget
-	var err error
-
 	log := log.FromContext(ctx).
 		WithName(logutil.LogLogger_managed_gitops).WithValues(
 		logutil.Log_Component, logutil.Log_Component_Appstudio_Controller,
@@ -76,12 +72,34 @@ func (r *DevsandboxDeploymentReconciler) Reconcile(ctx context.Context, req ctrl
 		},
 	}
 
-	if err = r.Client.Get(ctx, client.ObjectKeyFromObject(&spacerequest), &spacerequest); err != nil {
+	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(&spacerequest), &spacerequest); err != nil {
 		// Don't requeue if the requested object is not found/deleted.
 		if apierr.IsNotFound(err) {
 			return ctrl.Result{}, nil
 		}
 		return ctrl.Result{}, err
+	}
+
+	if spacerequest.DeletionTimestamp != nil {
+		// Skip processing SpaceRequests that are already in the process of being deleted.
+		return ctrl.Result{}, nil
+	}
+
+	if dtcLabel, exists := spacerequest.Labels[deploymentTargetClaimLabel]; !exists {
+		log.Info("SpaceRequest is missing an 'appstudio.openshift.io/dtc' label, so skipping.")
+		return ctrl.Result{}, nil
+
+	} else {
+		// Determine if the SpaceRequest is orphaned: that is, the SpaceRequest refers to a DTC that no longer exists.
+
+		spaceRequestDeleted, err := r.deleteSpaceRequestIfOrphaned(ctx, spacerequest, dtcLabel, log)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		if spaceRequestDeleted { // No more work to do
+			return ctrl.Result{}, nil
+		}
 	}
 
 	if len(spacerequest.Status.NamespaceAccess) > 1 {
@@ -90,16 +108,27 @@ func (r *DevsandboxDeploymentReconciler) Reconcile(ctx context.Context, req ctrl
 		return ctrl.Result{}, nil
 	}
 
-	if !doesSpaceRequestHaveReadyTrue(&spacerequest) {
+	if !doesSpaceRequestHaveReadyTrue(spacerequest) {
+		log.Info("SpaceRequest is not ready yet")
 		return ctrl.Result{}, nil
 	}
-	if dt, err = findMatchingDTForSpaceRequest(ctx, r.Client, &spacerequest); err != nil {
+
+	if len(spacerequest.Status.NamespaceAccess) == 0 {
+		// No Namespaces yet
+		log.Info("SpaceRequest is ready, but does not have a NamespaceAccess")
+		return ctrl.Result{}, nil
+	}
+
+	var dt *applicationv1alpha1.DeploymentTarget
+	var err error
+
+	if dt, err = findMatchingDTForSpaceRequest(ctx, r.Client, spacerequest); err != nil {
 		log.Error(err, "error while finding a dt that matches the SpaceRequest")
 		return ctrl.Result{}, err
 	}
 
 	if dt == nil {
-		dt, err = createDeploymentTargetForSpaceRequest(ctx, r.Client, &spacerequest, log)
+		dt, err = createDeploymentTargetForSpaceRequest(ctx, r.Client, spacerequest, log)
 		if err != nil {
 			log.Error(err, "failed to create the DeploymentTarget for a SpaceRequest")
 			return ctrl.Result{}, err
@@ -107,33 +136,115 @@ func (r *DevsandboxDeploymentReconciler) Reconcile(ctx context.Context, req ctrl
 
 		log.Info("DeploymentTarget has been created for SpaceRequest", "SpaceRequest.Name", spacerequest.Name, "SpaceRequest.Namespace", spacerequest.Namespace)
 		logutil.LogAPIResourceChangeEvent(dt.Namespace, dt.Name, dt, logutil.ResourceCreated, log)
-	}
 
-	log.Info("A DeploymentTarget for the SpaceRequest exists", "DeploymentTarget.Name", dt.Name, "Namespace", dt.Namespace)
+	} else {
+		log.Info("A DeploymentTarget for the SpaceRequest exists, no work needed.", "DeploymentTarget.Name", dt.Name, "Namespace", dt.Namespace)
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// findMatchingDTForSpaceRequest tries to find a DT that matches the given SpaceRequest in a namespace.
-func findMatchingDTForSpaceRequest(ctx context.Context, k8sClient client.Client, spacerequest *codereadytoolchainv1alpha1.SpaceRequest) (*applicationv1alpha1.DeploymentTarget, error) {
-	if dtc, _ := findMatchingDTCForSpaceRequest(ctx, k8sClient, spacerequest); dtc != nil {
-		dtList := applicationv1alpha1.DeploymentTargetList{}
-		opts := []client.ListOption{
-			client.InNamespace(spacerequest.Namespace),
-		}
+// deleteSpaceRequestIfOrphaned will delete a SpaceRequest if:
+// - The dev sandbox DeploymentTargetClass exist, and it has Delete Policy
+// - The DTC referenced by the SpaceRequest label does not exit
+//
+// Return true if the SpaceRequest was deleted, false otherwise
+func (r *DevsandboxDeploymentReconciler) deleteSpaceRequestIfOrphaned(ctx context.Context, spacerequest codereadytoolchainv1alpha1.SpaceRequest, dtcLabelFromSpaceRequest string, log logr.Logger) (bool, error) {
 
-		if err := k8sClient.List(ctx, &dtList, opts...); err != nil {
-			return nil, err
-		}
+	// Locate the Devsandbox DeploymentTargetClass
+	var dtClassList applicationv1alpha1.DeploymentTargetClassList
+	if err := r.Client.List(ctx, &dtClassList); err != nil {
+		return false, err
+	}
 
-		var dt *applicationv1alpha1.DeploymentTarget
-		for i, d := range dtList.Items {
-			if dtc.Name == string(d.Spec.ClaimRef) {
-				dt = &dtList.Items[i]
-				return dt, nil
-			}
+	var dtClassDevsandbox *applicationv1alpha1.DeploymentTargetClass
+
+	for i, dtClass := range dtClassList.Items {
+
+		if dtClass.Spec.Provisioner == applicationv1alpha1.Provisioner_Devsandbox {
+
+			dtClassDevsandbox = &dtClassList.Items[i]
 		}
 	}
+
+	if dtClassDevsandbox == nil {
+		// Devsandbox provisioner could not be located, so we should not delete the SpaceRequest
+		return false, nil
+	}
+
+	if dtClassDevsandbox.Spec.ReclaimPolicy != applicationv1alpha1.ReclaimPolicy_Delete {
+		// The reclaim policy of Devsandbox provisioner is not delete, so we should not delete the SpaceRequest
+		return false, nil
+	}
+
+	// Attempt to locate the DTC that references the SpaceRequest
+	dtcReferencedByLabel := applicationv1alpha1.DeploymentTargetClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dtcLabelFromSpaceRequest,
+			Namespace: spacerequest.Namespace,
+		},
+	}
+
+	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(&dtcReferencedByLabel), &dtcReferencedByLabel); err != nil {
+		if !apierr.IsNotFound(err) {
+			return false, err
+		}
+
+		// If the DeploymentTargetClaim doesn't exist, delete the SpaceRequest
+		if err := r.Client.Delete(ctx, &spacerequest); err != nil {
+			log.Error(err, "unable to delete SpaceRequest that is no longer referenced by DTC")
+			return false, err
+		}
+
+		log.Info("the DTC referenced by the SpaceRequest no longer exists, so deleted the SpaceRequest", "dtcLabel", dtcReferencedByLabel)
+
+		logutil.LogAPIResourceChangeEvent(spacerequest.Namespace, spacerequest.Name, &spacerequest, logutil.ResourceDeleted, log)
+
+		return true, nil
+
+	}
+
+	return false, nil
+}
+
+// doesSpaceRequestHaveReadyTrue checks if SpaceRequest condition is in Ready status.
+func doesSpaceRequestHaveReadyTrue(spacerequest codereadytoolchainv1alpha1.SpaceRequest) bool {
+	return condition.IsTrue(spacerequest.Status.Conditions, codereadytoolchainv1alpha1.ConditionReady)
+}
+
+// findMatchingDTForSpaceRequest tries to find a DT that matches the given SpaceRequest in a namespace.
+func findMatchingDTForSpaceRequest(ctx context.Context, k8sClient client.Client, spacerequest codereadytoolchainv1alpha1.SpaceRequest) (*applicationv1alpha1.DeploymentTarget, error) {
+
+	// 1) Find the DTC using the DTC label on the SpaceRequet
+	dtc, err := findMatchingDTCForSpaceRequest(ctx, k8sClient, spacerequest)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if dtc == nil {
+		return nil, nil
+	}
+
+	// 2) The DTC was found, so find the DT that reference that DTC in its claimref
+
+	dtList := applicationv1alpha1.DeploymentTargetList{}
+	opts := []client.ListOption{
+		client.InNamespace(spacerequest.Namespace),
+	}
+
+	if err := k8sClient.List(ctx, &dtList, opts...); err != nil {
+		return nil, err
+	}
+
+	for i, dt := range dtList.Items {
+		if dtc.Name == dt.Spec.ClaimRef {
+
+			matchingDT := &dtList.Items[i]
+			return matchingDT, nil
+		}
+	}
+
 	return nil, nil
 }
 
@@ -148,17 +259,18 @@ func HasLabel(object client.Object, label string) bool {
 }
 
 // findMatchingDTCForSpaceRequest tries to find the DTC that matches a given SpaceRequest.
-func findMatchingDTCForSpaceRequest(ctx context.Context, k8sClient client.Client, spacerequest *codereadytoolchainv1alpha1.SpaceRequest) (*applicationv1alpha1.DeploymentTargetClaim, error) {
-	if !HasLabel(spacerequest, deploymentTargetClaimLabel) {
-		return nil, fmt.Errorf("no 'appstudio.openshift.io/dtc' label is set for spacerequest %s %s", spacerequest.Name, spacerequest.Namespace)
+func findMatchingDTCForSpaceRequest(ctx context.Context, k8sClient client.Client, spacerequest codereadytoolchainv1alpha1.SpaceRequest) (*applicationv1alpha1.DeploymentTargetClaim, error) {
+	if !HasLabel(&spacerequest, deploymentTargetClaimLabel) {
+		return nil, fmt.Errorf("no 'appstudio.openshift.io/dtc' label is set for spacerequest '%s' '%s'", spacerequest.Name, spacerequest.Namespace)
 	}
-	dtc := &applicationv1alpha1.DeploymentTargetClaim{}
-	err := k8sClient.Get(ctx, types.NamespacedName{
-		Namespace: spacerequest.Namespace,
-		Name:      spacerequest.Labels[deploymentTargetClaimLabel],
-	}, dtc)
-	if err != nil {
-		return nil, err
+	dtc := &applicationv1alpha1.DeploymentTargetClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: spacerequest.Namespace,
+			Name:      spacerequest.Labels[deploymentTargetClaimLabel],
+		},
+	}
+	if err := k8sClient.Get(ctx, client.ObjectKeyFromObject(dtc), dtc); err != nil {
+		return nil, fmt.Errorf("unable to locate DTC with name '%s' in Namespace '%s': %w", dtc.Name, dtc.Namespace, err)
 	}
 	return dtc, nil
 }
@@ -182,7 +294,7 @@ func newDeploymentTarget(deploymentTargetClassName applicationv1alpha1.Deploymen
 		},
 	}
 
-	// Set AllowInsecureSkipTLSVerify field of DT to True ,if it is a dev cluster.
+	// Set AllowInsecureSkipTLSVerify field of DT to True, if it is a dev cluster.
 	if strings.EqualFold(os.Getenv("DEV_ONLY_IGNORE_SELFSIGNED_CERT_IN_DEPLOYMENT_TARGET"), "true") {
 		deploymentTarget.Spec.KubernetesClusterCredentials.AllowInsecureSkipTLSVerify = true
 	}
@@ -192,11 +304,11 @@ func newDeploymentTarget(deploymentTargetClassName applicationv1alpha1.Deploymen
 
 // createDeploymentTargetForSpaceRequest creates and returns a new DeploymentTarget
 // If it's not possible to create it and set the SpaceRequest as the owner, an error will be returned
-func createDeploymentTargetForSpaceRequest(ctx context.Context, client client.Client, spacerequest *codereadytoolchainv1alpha1.SpaceRequest, log logr.Logger) (*applicationv1alpha1.DeploymentTarget, error) {
+func createDeploymentTargetForSpaceRequest(ctx context.Context, client client.Client, spacerequest codereadytoolchainv1alpha1.SpaceRequest, log logr.Logger) (*applicationv1alpha1.DeploymentTarget, error) {
 
 	dtc, err := findMatchingDTCForSpaceRequest(ctx, client, spacerequest)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to locate matching DTC for SpaceRequest: %w", err)
 	}
 
 	deploymentTarget := newDeploymentTarget(
@@ -237,7 +349,5 @@ func createDeploymentTargetForSpaceRequest(ctx context.Context, client client.Cl
 func (r *DevsandboxDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&codereadytoolchainv1alpha1.SpaceRequest{}).
-		WithEventFilter(predicate.Or(
-			spaceRequestReadyPredicate())).
 		Complete(r)
 }
